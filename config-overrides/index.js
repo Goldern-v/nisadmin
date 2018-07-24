0f9e54e80eaef9b091e7a05368c962ef9087b720
varHarrie <varharrie@gmail.com>
2018-07-24 14:00:32 +0800

    Improve configurations for supporting styled-components

--- config-overrides/index.js
+++ config-overrides/index.js
@@ -1,9 +1,18 @@
+const rewireStyledComponents = require('react-app-rewire-styled-components')
 const rewireTypescript = require('react-app-rewire-typescript')
 const rewireTsJest = require('react-app-rewire-ts-jest')
 
+const tsOptions = require('./typescript-options')
+
 module.exports = {
   webpack: (config, env) => {
-    config = rewireTypescript(config, env)
+    config = rewireStyledComponents(config, env)
+
+    if (env === 'development') {
+      config = rewireTypescript(config, env, tsOptions)
+    } else {
+      config = rewireTypescript(config, env)
+    }
 
     return config
   },
