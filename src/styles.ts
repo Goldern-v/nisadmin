df999b22d9938f74aeb54b44a50a07c0860c4920
varHarrie <varharrie@gmail.com>
2018-07-24 17:23:52 +0800

    Add styles

---
+++ src/styles.ts
@@ -0,0 +1,27 @@
+import { injectGlobal } from 'styled-components'
+
+// tslint:disable-next-line:no-unused-expression
+injectGlobal`
+  html, body, div, span,
+  h1, h2, h3, h4, h5, h6, p, pre,
+  a, code, i, dl, dt, dd, ol, ul, li,
+  form, label, input, button, textarea,
+  table, tbody, tfoot, thead, tr, th, td,
+  canvas, footer, header, menu, nav, section, summary
+  {
+    box-sizing: border-box;
+  }
+
+  html {
+    height: 100%;
+  }
+
+  body {
+    margin: 0px;
+    height: 100%;
+  }
+
+  #root {
+    height: 100%;
+  }
+`
