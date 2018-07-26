4ae3d740dfd38bf03c25ed031ccb9cb57d5a4e1e
varHarrie <varharrie@gmail.com>
2018-07-26 10:37:13 +0800

    Add antd

--- config-overrides/index.js
+++ config-overrides/index.js
@@ -2,11 +2,13 @@ const rewireStyledComponents = require('react-app-rewire-styled-components')
 const rewireTypescript = require('react-app-rewire-typescript')
 const rewireTsJest = require('react-app-rewire-ts-jest')
 
+const rewireAntd = require('./rewire-antd')
 const tsOptions = require('./typescript-options')
 
 module.exports = {
   webpack: (config, env) => {
     config = rewireStyledComponents(config, env)
+    config = rewireAntd(config, env)
 
     if (env === 'development') {
       config = rewireTypescript(config, env, tsOptions)
