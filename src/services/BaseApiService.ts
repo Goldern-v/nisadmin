dbf4c6f75e707ce72f932b1a0e58a90977559a7d
varHarrie <varharrie@gmail.com>
2018-08-22 17:56:38 +0800

    Make functions protected

--- src/services/BaseApiService.ts
+++ src/services/BaseApiService.ts
@@ -5,35 +5,35 @@ import http from '../libs/http'
 
 export default abstract class BaseApiService {
 
-  public request<T = any> (config: AxiosRequestConfig): Promise<T> {
+  protected request<T = any> (config: AxiosRequestConfig): Promise<T> {
     return http.request(config) as any
   }
 
-  public get<T = any> (url: string, config?: AxiosRequestConfig): Promise<T> {
+  protected get<T = any> (url: string, config?: AxiosRequestConfig): Promise<T> {
     return http.get(url, config) as any
   }
 
-  public head<T = any> (url: string, config?: AxiosRequestConfig): Promise<T> {
+  protected head<T = any> (url: string, config?: AxiosRequestConfig): Promise<T> {
     return http.head(url, config) as any
   }
 
-  public post<T = any> (url: string, data?: any, config?: AxiosRequestConfig): Promise<T> {
+  protected post<T = any> (url: string, data?: any, config?: AxiosRequestConfig): Promise<T> {
     return http.post(url, data, config) as any
   }
 
-  public put<T = any> (url: string, data?: any, config?: AxiosRequestConfig): Promise<T> {
+  protected put<T = any> (url: string, data?: any, config?: AxiosRequestConfig): Promise<T> {
     return http.put(url, data, config) as any
   }
 
-  public patch<T = any> (url: string, data?: any, config?: AxiosRequestConfig): Promise<T> {
+  protected patch<T = any> (url: string, data?: any, config?: AxiosRequestConfig): Promise<T> {
     return http.patch(url, data, config) as any
   }
 
-  public delete<T = any> (url: string, config?: AxiosRequestConfig): Promise<T> {
+  protected delete<T = any> (url: string, config?: AxiosRequestConfig): Promise<T> {
     return http.delete(url, config) as any
   }
 
-  public stringify (obj: any, option?: qs.IStringifyOptions): string {
+  protected stringify (obj: any, option?: qs.IStringifyOptions): string {
     return qs.stringify(obj, option)
   }
 
