fb88f7b5f953c701ee437bcd9e1334bfa322910f
weijun-lai <weijun.lai@uqconnect.edu.au>
2019-03-26 19:29:11 +0800

    移除表格组件，改用 蚂蚁 表格组件，完成 表格数据显示， 排班小计， 排班颜色变换。

--- src/modules/schedule/views/components/ScheduleTable/ScheduleTable.tsx
+++ src/modules/schedule/views/components/ScheduleTable/ScheduleTable.tsx
@@ -1,11 +1,8 @@
 import styled from 'styled-components'
 import React, { useState, useEffect } from 'react'
 import { RouteComponentProps } from 'react-router'
-import Th from './Th'
-import Tr from './Tr'
-import Col from './Col'
 
-import { Card } from 'antd'
+import { Card, Table, Tag } from 'antd'
 const { Meta } = Card
 
 import emitter from 'src/libs/ev'
@@ -24,162 +21,129 @@ export interface TdItem {
   value: string
 }
 
-interface TableData {
-  th: ThItem[]
-  tr: TdItem[]
-}
-const tableData: TableData = {
-  th: [
-    {
-      value: '序号',
-      width: '5%'
-    },
-    {
-      value: '工号',
-      width: '5%'
-    },
-    {
-      value: '姓名',
-      width: '5%'
-    },
-    {
-      value: '层级',
-      width: '5%'
-    },
-    {
-      value: '职称',
-      width: '5%'
-    },
-    {
-      value: '周一',
-      width: '6%'
-    },
-    {
-      value: '周二',
-      width: '6%'
-    },
-    {
-      value: '周三',
-      width: '6%'
-    },
-    {
-      value: '周四',
-      width: '6%'
-    },
-    {
-      value: '周五',
-      width: '6%'
-    },
-    {
-      value: '周六',
-      width: '6%'
-    },
-    {
-      value: '周日',
-      width: '6%'
-    },
-    {
-      value: '备注',
-      width: '20%'
-    },
-    {
-      value: '本周工时(小时)',
-      width: '7%'
-    },
-    {
-      value: '状态',
-      width: '10%'
-    }
-  ],
-  tr: [
-    {
-      key: 'id',
-      value: 'id'
-    },
-    {
-      key: 'empNo',
-      value: 'empNo'
-    },
-    {
-      key: 'empName',
-      value: 'empName'
-    },
-    {
-      key: 'currentLevel',
-      value: 'currentLevel'
-    },
-    {
-      key: 'title',
-      value: 'title'
-    },
-    {
-      key: 'rangeName1',
-      value: 'rangeName1'
-    },
-    {
-      key: 'rangeName2',
-      value: 'rangeName2'
-    },
-    {
-      key: 'rangeName3',
-      value: 'rangeName3'
-    },
-    {
-      key: 'rangeName4',
-      value: 'rangeName4'
-    },
-    {
-      key: 'rangeName5',
-      value: 'rangeName5'
-    },
-    {
-      key: 'rangeName6',
-      value: 'rangeName6'
-    },
-    {
-      key: 'rangeName7',
-      value: 'rangeName7'
-    },
-    {
-      key: 'remark',
-      value: 'remark'
-    },
-    {
-      key: 'thisWeekHour',
-      value: 'thisWeekHour'
-    },
-    {
-      key: 'status',
-      value: 'status'
-    }
-  ]
-}
+const getTextColor = (text: any) =>
+  text.indexOf('N') > -1 ? (
+    <span>
+      <Tag color={'red'} key={text}>
+        {text.toUpperCase()}
+      </Tag>
+    </span>
+  ) : text.indexOf('休') > -1 ? (
+    <span>
+      <Tag color={'blue'} key={text}>
+        {text.toUpperCase()}
+      </Tag>
+    </span>
+  ) : (
+    <span>{text}</span>
+  )
+
+const columns = [
+  {
+    title: '序号',
+    dataIndex: 'id',
+    width: '5%'
+  },
+  {
+    title: '工号',
+    dataIndex: 'empNo',
+    width: '5%'
+  },
+  {
+    title: '姓名',
+    dataIndex: 'empName',
+    width: '5%'
+  },
+  {
+    title: '层级',
+    dataIndex: 'currentLevel',
+    width: '5%'
+  },
+  {
+    title: '职称',
+    dataIndex: 'title',
+    width: '5%'
+  },
+  {
+    title: '周一',
+    dataIndex: 'rangeName1',
+    width: '6%',
+    render: (text: any) => getTextColor(text)
+  },
+  {
+    title: '周二',
+    dataIndex: 'rangeName2',
+    width: '6%',
+    render: (text: any) => getTextColor(text)
+  },
+  {
+    title: '周三',
+    dataIndex: 'rangeName3',
+    width: '6%',
+    render: (text: any) => getTextColor(text)
+  },
+  {
+    title: '周四',
+    dataIndex: 'rangeName4',
+    width: '6%',
+    render: (text: any) => getTextColor(text)
+  },
+  {
+    title: '周五',
+    dataIndex: 'rangeName5',
+    width: '6%',
+    render: (text: any) => getTextColor(text)
+  },
+  {
+    title: '周六',
+    dataIndex: 'rangeName6',
+    width: '6%',
+    render: (text: any) => getTextColor(text)
+  },
+  {
+    title: '周日',
+    dataIndex: 'rangeName7',
+    width: '6%',
+    render: (text: any) => getTextColor(text)
+  },
+  {
+    title: '备注',
+    dataIndex: 'remark',
+    width: '20%'
+  },
+  {
+    title: '本周工时(小时)',
+    dataIndex: 'thisWeekHour',
+    width: '7%'
+  },
+  {
+    title: '状态',
+    dataIndex: 'status',
+    width: '10%'
+  }
+]
 
 export default function ScheduleTable () {
   const [count, setCount] = useState(0)
-  const [scheduleList, setscheduleList] = useState([
-    [
-      {
-        key: '',
-        value: ''
-      }
-    ]
-  ])
+  const [footer, setFooter] = useState('')
+  const [scheduleList, setScheduleList] = useState([])
+
   useEffect(() => {
     console.log(count, setCount)
-    setscheduleList([])
+    setScheduleList([])
+    setFooter('排班小计')
 
     let eventEmitterClean = emitter.addListener('清空排班记录', () => {
-      setscheduleList([])
+      setScheduleList([])
     })
 
     let eventEmitter = emitter.addListener('本周排班记录', (scheduleData) => {
       console.log('接收:本周排班记录event', scheduleData, scheduleList)
       // schShiftUser
-      let trArry = {}
-      let newList = [[]]
-      scheduleData.schShiftUser.map((nurse: any, index: number) => {
-        console.log('nurse', index, nurse.empName, nurse)
+      let tr = {}
+      let newList = new Array()
+      scheduleData.schShiftUser.map((nurse: any, shcIndex: number) => {
+        console.log('nurse', shcIndex, nurse.empName, nurse)
         let getRangeName = (range: any, i: number) => {
           let result = ''
           try {
@@ -189,8 +153,20 @@ export default function ScheduleTable () {
           }
           return result
         }
-        trArry = {
-          id: nurse.id || '',
+        let getStatus = () => {
+          // status   0代表暂存，1代表发布
+          switch (nurse.status) {
+            case '0':
+              return '暂存'
+            case '1':
+              return '发布'
+            default:
+              return '未设置排班'
+          }
+        }
+
+        tr = {
+          id: shcIndex + 1 || '',
           empNo: nurse.empNo || '',
           empName: nurse.empName || '',
           currentLevel: nurse.currentLevel || '',
@@ -204,34 +180,88 @@ export default function ScheduleTable () {
           rangeName7: getRangeName(nurse.settingDtos, 6) || '',
           remark: nurse.remark || '',
           thisWeekHour: nurse.thisWeekHour || '',
-          status: nurse.status || ''
+          status: getStatus() || ''
         }
-        let tr = []
-        for (let key in trArry) {
-          if (trArry.hasOwnProperty(key)) {
-            let value = (trArry as any)[key]
-            tr.push({ key: key, value: value })
-          }
-        }
-        console.log('tr', tr, newList)
+        // console.log('tr', tr)
         newList.push(tr as any)
       })
-      setscheduleList(newList)
+      // 统计
+      statisticFooter(newList)
+      // 补空行
+      let diff = 10 - newList.length
+      if (diff > 0) {
+        for (let j = 0; j < diff; j++) {
+          newList.push({
+            id: '',
+            empNo: '',
+            empName: '',
+            currentLevel: '',
+            title: '',
+            rangeName1: '',
+            rangeName2: '',
+            rangeName3: '',
+            rangeName4: '',
+            rangeName5: '',
+            rangeName6: '',
+            rangeName7: '',
+            remark: '',
+            thisWeekHour: '',
+            status: ''
+          })
+        }
+      }
+      setScheduleList(newList as any)
     })
     console.log('eventEmitter', eventEmitter, eventEmitterClean)
   }, [])
+
+  function statisticFooter (list: any) {
+    console.log('统计', list)
+    let workhour = 0
+    let rangeNames = new Array()
+    let rangeObj = new Object()
+
+    list.map((item: any) => {
+      if (item.thisWeekHour) {
+        workhour += item.thisWeekHour
+      }
+      for (let ii = 1; ii < 8; ii++) {
+        let element = (item as any)['rangeName' + ii]
+        if (element && element.length > 0) {
+          if (rangeNames.indexOf(element) === -1) {
+            //
+            (rangeObj as any)[element] = 1
+          } else {
+            (rangeObj as any)[element] += 1
+          }
+          rangeNames.push(element)
+        }
+      }
+    })
+
+    let rangeSum = ''
+    for (const key in rangeObj) {
+      if (rangeObj.hasOwnProperty(key)) {
+        const element = (rangeObj as any)[key]
+        rangeSum += `${key}(${element})，`
+      }
+    }
+    rangeSum = rangeSum.trim()
+
+    console.log('统计', workhour, rangeNames, rangeObj)
+    // 排班小计：A1(3) 、A2(2)、N1(2)、...............，工时40小时。
+    setFooter(`排班小计：${rangeSum}工时${workhour}小时。`)
+    return ''
+  }
+
   return (
     <Wrapper>
       {scheduleList.length > 0 && (
-        <table>
-          <Col th={tableData.th} />
-          <thead>
-            <Th th={tableData.th} />
-          </thead>
-          {scheduleList.map((nurse, index) => (
-            <Tr tr={nurse} />
-          ))}
-        </table>
+        <ScheduleCon>
+          <ScheduleTableCon>
+            <Table columns={columns} dataSource={scheduleList} pagination={{ pageSize: 10 }} footer={() => footer} />
+          </ScheduleTableCon>
+        </ScheduleCon>
       )}
       {scheduleList.length === 0 && (
         <NoScheduleCon>
@@ -258,6 +288,7 @@ const Wrapper = styled.div`
     border: 1px solid #d9d8d8;
     min-height: 30px;
     text-align: center;
+    height: 30px;
   }
   th {
     background: #f8f8f8;
@@ -277,4 +308,19 @@ const CardBox = styled.div`
   position: relative;
   top: 35%;
   background: green;
+`
+
+const ScheduleCon = styled.div`
+  height: calc(100vh - 300px);
+  overflow: auto;
+  margin-top: 0px;
+  background: #fff;
+`
+
+const ScheduleTableCon = styled.div`
+  height: calc(100vh - 300px);
+  overflow: auto;
+  margin-top: 0px;
+  background: #fff;
+  /* background: red; */
 `
