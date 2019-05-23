26520f12f6b8a5d14830885b1264f7d2da58e8f3
fermincn <fm91991@163.com>
2019-05-23 10:23:45 +0800

    标识

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,6 +4,7 @@ module.exports = function(app) {
   app.use(
     proxy('/crNursing/api', {
       // target: 'http://120.197.141.41:9094',
+      //广
       // target: 'http://192.168.2.144:8080',
       target: 'http://120.25.105.45:9864',
       //正式环境
