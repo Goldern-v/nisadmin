f05dae367e6101ce82a7505f033c5cf5a433cb2d
LINZJAI <969060748@qq.com>
2019-07-05 12:00:04 +0800

    合并

--- src/components/common.tsx
+++ src/components/common.tsx
@@ -4,18 +4,33 @@ import styled from 'styled-components'
 export const Place = styled.div`
   flex: 1;
 `
+export const ScrollBox = styled.ul`
+  &::-webkit-scrollbar {
+    width: 8px;
+    height: 8px;
+    background-color: #eaeaea;
+  }
+  &::-webkit-scrollbar-track {
+    border-radius: 50px;
+    background-color: #eaeaea;
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 50px;
+    background-color: #c2c2c2;
+  }
+`
 export const ScrollUl = styled.ul`
   &::-webkit-scrollbar {
-      width: 8px;
-      height: 8px;
-      background-color: #eaeaea;
-    }
-    &::-webkit-scrollbar-track {
-      border-radius: 50px;
-      background-color: #eaeaea;
-    }
-    &::-webkit-scrollbar-thumb {
-      border-radius: 50px;
-      background-color: #c2c2c2;
-    }
+    width: 8px;
+    height: 8px;
+    background-color: #eaeaea;
+  }
+  &::-webkit-scrollbar-track {
+    border-radius: 50px;
+    background-color: #eaeaea;
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 50px;
+    background-color: #c2c2c2;
+  }
 `
