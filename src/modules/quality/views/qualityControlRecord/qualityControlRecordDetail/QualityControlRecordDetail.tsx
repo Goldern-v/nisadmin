a183078b6d6bc7ed36c0d51180f1d4df88e8cda2
fangming <fangming@cr-health.com>
2019-07-19 16:13:46 +0800

    滚动调整

--- src/modules/quality/views/qualityControlRecord/qualityControlRecordDetail/QualityControlRecordDetail.tsx
+++ src/modules/quality/views/qualityControlRecord/qualityControlRecordDetail/QualityControlRecordDetail.tsx
@@ -54,20 +54,58 @@ const MidConScrollCon = styled.div`
   display: flex;
   align-items: stretch;
   overflow-y: auto;
+  ::-webkit-scrollbar {
+    /*滚动条整体样式*/
+    width: 6px; /*高宽分别对应横竖滚动条的尺寸*/
+    height: 10px;
+  }
+  ::-webkit-scrollbar-thumb {
+    /*滚动条里面小方块*/
+    border-radius: 5px;
+    box-shadow: inset 0 0 8px rgba(0, 0, 0, 0.2);
+    background: rgba(0, 0, 0, 0.2);
+  }
+  /*定义滚动条轨道 内阴影+圆角*/
+  ::-webkit-scrollbar-track {
+    /*滚动条里面轨道*/
+    box-shadow: inset 0 0 5px #ffffff;
+    border-radius: 5px;
+    background-color: #ffffff;
+  }
   /* background-color: #fff; */
   /* height: 150%; */
-  flex-basis: auto;
+  /* flex-basis: auto; */
 `
 const MidLeftCon = styled.div`
   box-sizing: border-box;
   padding: 20px 153px;
   flex: 1;
   width: 0;
-  min-height: 100%;
+  height: 100%;
+  overflow-y: auto;
+
   /* height: auto; */
   /* border-right: 1px solid gray; */
   background-color: #eeeeee;
   align-items: stretch;
+  ::-webkit-scrollbar {
+    /*滚动条整体样式*/
+    width: 6px; /*高宽分别对应横竖滚动条的尺寸*/
+    height: 10px;
+  }
+  ::-webkit-scrollbar-thumb {
+    /*滚动条里面小方块*/
+    border-radius: 5px;
+    box-shadow: inset 0 0 8px rgba(0, 0, 0, 0.2);
+    background: rgba(0, 0, 0, 0.2);
+  }
+  /*定义滚动条轨道 内阴影+圆角*/
+  ::-webkit-scrollbar-track {
+    /*滚动条里面轨道*/
+    box-shadow: inset 0 0 5px #ffffff;
+    border-radius: 5px;
+    background-color: #ffffff;
+  }
   .QualityControlRecordDetailMidLeft__Con-sc-1yldaue-0 {
     /* border-right: 1px solid #e8f6f1; */
     /* background-color: #eeeeee; */
