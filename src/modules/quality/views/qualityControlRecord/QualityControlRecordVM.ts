7b8ce4424704e10b39d10f7b36946dd259f1de57
fangming <fangming@cr-health.com>
2019-07-19 15:49:21 +0800

    数据更正

--- src/modules/quality/views/qualityControlRecord/QualityControlRecordVM.ts
+++ src/modules/quality/views/qualityControlRecord/QualityControlRecordVM.ts
@@ -16,18 +16,18 @@ class QualityControlRecordVM {
   @observable public filterText: string = ''
   @computed
   public get getDefaultName() {
-    let formSelectList = [...this.stateSelectList]
+    let formSelectList = [...this.formSelectList]
     if (!formSelectList[0]) {
       formSelectList[0] = {}
     }
     if (!formSelectList[0].name) {
-      formSelectList[0].name = '一、基础护理质量评价表'
+      formSelectList[0].name = '基础护理质量评价表'
     }
     return formSelectList[0].name
   }
   @computed
   public get getDefaultStateName() {
-    let formSelectList = [...this.formSelectList]
+    let formSelectList = [...this.stateSelectList]
     if (!formSelectList[0]) {
       formSelectList[0] = {}
     }
