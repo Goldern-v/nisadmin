8b7fe95583de23e4513191b49dc0594e144109a9
fangming <fangming@cr-health.com>
2019-07-20 16:50:57 +0800

    空白去阴影

--- src/modules/quality/views/qualityControlRecord/qualityControlRecordDetail/QualityControlRecordDetail.tsx
+++ src/modules/quality/views/qualityControlRecord/qualityControlRecordDetail/QualityControlRecordDetail.tsx
@@ -110,30 +110,13 @@ const MidLeftCon = styled.div`
   /* border-right: 1px solid gray; */
   background-color: #eeeeee;
   align-items: stretch;
-  ::-webkit-scrollbar {
-    /*滚动条整体样式*/
-    width: 6px; /*高宽分别对应横竖滚动条的尺寸*/
-    height: 10px;
-  }
-  ::-webkit-scrollbar-thumb {
-    /*滚动条里面小方块*/
-    border-radius: 5px;
-    box-shadow: inset 0 0 8px rgba(0, 0, 0, 0.2);
-    background: rgba(0, 0, 0, 0.2);
-  }
-  /*定义滚动条轨道 内阴影+圆角*/
-  ::-webkit-scrollbar-track {
-    /*滚动条里面轨道*/
-    box-shadow: inset 0 0 5px #ffffff;
-    border-radius: 5px;
-    background-color: #ffffff;
-  }
   .QualityControlRecordDetailMidLeft__Con-sc-1yldaue-0 {
-    /* border-right: 1px solid #e8f6f1; */
-    /* background-color: #eeeeee; */
-    /* #EEEEEE */
-    background: rgba(255, 255, 255, 1);
-    box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.5);
+    /* ui的 */
+    /* background: rgba(255, 255, 255, 1);
+    box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.5); */
+    /* 空白的 */
+    background: #fff;
+    border: 1px solid #ddd;
   }
 `
 const MidRightCon = styled.div`
