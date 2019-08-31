9f510ec242cdaf01a4e7731cc7e6d5ac5bb0b93c
LINZJAI <969060748@qq.com>
2019-08-31 16:13:44 +0800

    修改厚街档案导航bug

--- src/modules/nurseFiles/view/nurseFiles-hj/views/nurseFileDetail/components/TopCon.tsx
+++ src/modules/nurseFiles/view/nurseFiles-hj/views/nurseFileDetail/components/TopCon.tsx
@@ -49,7 +49,7 @@ export default observer(function TopCon() {
       <BreadcrumbCon>
         <Breadcrumb>
           <Breadcrumb.Item>
-            <A onClick={() => history.push('/nurseFilesList')}>护士档案</A>
+            <A onClick={() => history.push('/nurseFile/onTheJob')}>护士档案</A>
           </Breadcrumb.Item>
           <Breadcrumb.Item>档案详情</Breadcrumb.Item>
         </Breadcrumb>
