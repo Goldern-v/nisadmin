68fbdd850fbde43485c81e1c9108c1311229b622
吴敏 <wumin@wumindeMacBook-Pro.local>
2019-10-31 09:42:06 +0800

    查房修改

--- src/modules/quality/views/checkWard/view/details/components/Header.tsx
+++ src/modules/quality/views/checkWard/view/details/components/Header.tsx
@@ -109,7 +109,7 @@ export default function Header(props: Props) {
           </div>
         </div>
         <div className='topHeaderStatus'>
-          状态：<span style={{ color: '#6767ff' }}>{titleStatus()}</span>
+          状态：<span style={{ color: '#6767ff' }}>{Title.nextNodePendingName}</span>
         </div>
       </TopHeader>
       <hlbModal.Component />
