79f86c6b2523cd7cda589b6e64dc0c2b3bfe007b
LINZJAI <969060748@qq.com>
2019-11-29 16:57:15 +0800

    修改

--- src/modules/personnelManagement/views/arrangeHome/components/arrangeSheet/TotalCell.tsx
+++ src/modules/personnelManagement/views/arrangeHome/components/arrangeSheet/TotalCell.tsx
@@ -5,6 +5,7 @@ import { observer } from "src/vendors/mobx-react-lite.ts";
 import { sheetViewModal } from "../../viewModal/SheetViewModal";
 import { ArrangeItem } from "../../types/Sheet";
 import { cloneJson } from "src/utils/json/clone";
+import moment from "moment";
 export interface Props {
   id: any;
 }
@@ -21,6 +22,29 @@ export default observer(function TotalCell(props: Props) {
   let total = list.reduce((total: any, current: ArrangeItem | any) => {
     return total + Number(current.effectiveTime);
   }, 0);
+
+  /** 如果存在一周7天都是 */
+
+  let weekObj: any = {};
+
+  for (let item of list) {
+    if (sheetViewModal.weekArrangeNameList.includes(item.rangeName)) {
+      let num = moment(item.workDate).week();
+      if (weekObj[num]) {
+        weekObj[num].push(num);
+      } else {
+        weekObj[num] = [];
+        weekObj[num].push(num);
+      }
+    }
+  }
+
+  for (let key in weekObj) {
+    if (weekObj[key].length == 7) {
+      total -= 15;
+    }
+  }
+
   return <Wrapper>{Number(total).toFixed(1)}</Wrapper>;
 });
 const Wrapper = styled.div``;
