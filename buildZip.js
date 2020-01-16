aeea9f6fd14bb85a577d3f7c88ee7ac5a1826307
LINZJAI <969060748@qq.com>
2020-01-16 16:41:03 +0800

    打包自动生成压缩包

---
+++ buildZip.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const fs = require("fs");
+const chalk = require("chalk");
+// 这个是主角
+const compressing = require("compressing");
+const moment = require("moment");
+
+let hospitalName = process.env.REACT_APP_HOSPITAL_NAME
+
+let sourcePath = 'build/'
+let zipPath = `build/护理管理_${hospitalName}_${moment().format("YYYY_MM_DD_HH_mm")}.zip`
+
+
+
+
+const resolve = dir => path.join(__dirname, "./", dir);
+const publishPath = resolve("publish");
+const zipName = (() => zipPath)();
+
+// 判断是否存在当前publish路径，没有就新增
+if (!fs.existsSync(publishPath)) {
+  fs.mkdirSync(publishPath);
+}
+
+compressing.zip
+  .compressDir(resolve(sourcePath), resolve(zipName))
+  .then(() => {
+    console.log(chalk.yellow(`Tip: 文件压缩成功，已压缩至【${resolve(zipName)}】`));
+  })
+  .catch(err => {
+    console.log(chalk.red("Tip: 压缩报错"));
+    console.error(err);
+  });
