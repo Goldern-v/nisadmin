706bd235c2e4f1ae53c4630b567a1987775c6556
shiwei <shiwei@cr-health.com>
2020-03-24 14:09:05 +0800

    1.健康宣教字典编辑调整 2.学习培训 路由菜单匹配调整

--- src/modules/continuingEdu/views/promotionSetting/components/RequestEditModal.tsx
+++ src/modules/continuingEdu/views/promotionSetting/components/RequestEditModal.tsx
@@ -1,5 +1,5 @@
-import styled from "styled-components";
-import React, { useState, useEffect, useLayoutEffect } from "react";
+import styled from "styled-components"
+import React, { useState, useEffect, useLayoutEffect } from "react"
 import {
   Modal,
   Tabs,
@@ -10,89 +10,117 @@ import {
   // Select,
   // message,
   Transfer
-} from "antd";
-import { ModalComponentProps } from "src/libs/createModal";
-import { observer } from 'mobx-react-lite'
-import { promotionSettingModel } from '../model/PromotionSettingModel'
+} from "antd"
+import { ModalComponentProps } from "src/libs/createModal"
+import { observer } from "mobx-react-lite"
+import { promotionSettingModel } from "../model/PromotionSettingModel"
+import { promotionSettingService } from './../api/PromotionSettingService'
 
-export interface Props extends ModalComponentProps { }
+export interface Props extends ModalComponentProps {
+  defaultLevelSort?: string
+}
 
 export default observer(function RequestEditModal(props: Props) {
-  let { config } = promotionSettingModel
+  const { defaultLevelSort } = props
+  const [levelSort, setLevelSort] = useState('')
+  const { levelList } = promotionSettingModel
   let { visible, onCancel } = props
   let [loading, setLoading] = useState(false)
-  let [activeIdx, setActiveIdx] = useState('0')
   let [targetKeys, setTargetKeys] = useState([] as string[])
   let [selectedKeys, setSelectedKeys] = useState([] as string[])
+  let [promotionList, setPromotionList] = useState([] as string[])
 
-  useLayoutEffect(() => { }, [visible])
+  useLayoutEffect(() => {
+    if (visible) {
+      setLevelSort(defaultLevelSort || '')
+      getPromotionList()
+    }
+  }, [visible])
 
   const handleOk = () => { }
 
+  const getPromotionList = (callback?: Function) => {
+    setLoading(true)
+    setSelectedKeys([])
+    setTargetKeys([])
+
+    let reqArr = [
+      promotionSettingService.getAllPromoteRequest(),
+    ]
+
+    Promise.all(reqArr).then(res => {
+      setLoading(false)
+      let newList = res[0].data || []
+      setPromotionList(newList)
+      callback && callback(newList)
+    }, () => setLoading(false))
+  }
+
   const handleTabChange = (idx: string) => {
-    setActiveIdx(idx)
+    setLevelSort(idx)
     setSelectedKeys([])
     setTargetKeys([])
   }
 
-  const levelList = [
-    {
-      title: 'N0升N1',
-      index: 0
-    },
-    {
-      title: 'N1升N2',
-      index: 1
-    }
-  ]
-
-  return <Modal
-    confirmLoading={loading}
-    bodyStyle={{ padding: 0, paddingBottom: '10px' }}
-    title="晋升要求编辑"
-    visible={visible}
-    onCancel={onCancel}
-    onOk={handleOk}
-    okText="确定"
-    centered>
-    <Wrapper>
-      <Tabs
-        activeKey={activeIdx}
-        type="card"
-        style={{ marginTop: '5px' }}
-        onChange={(idx: string) => handleTabChange(idx)}>
-        {levelList.map((item: any) =>
-          <Tabs.TabPane key={item.index} tab={item.title} disabled={loading}></Tabs.TabPane>)}
-      </Tabs>
-      <Spin spinning={loading}>
-        <div className="tabs-content">
-          <Transfer
-            targetKeys={targetKeys}
-            selectedKeys={selectedKeys}
-            render={item => item.title}
-            listStyle={{ width: '225px' }}
-            onSelectChange={(keys: string[], keys1: string[]) => setSelectedKeys(keys.concat(keys1))}
-            onChange={(keys: any) => setTargetKeys(keys)}
-            dataSource={Object.keys(config).map((key: string) => {
-              let item = config[key]
-              return {
-                key,
-                title: item.title,
-                description: item.title
+  return (
+    <Modal
+      width={600}
+      confirmLoading={loading}
+      bodyStyle={{ padding: 0, paddingBottom: "10px" }}
+      title="晋升要求编辑"
+      visible={visible}
+      onCancel={onCancel}
+      onOk={handleOk}
+      okText="确定"
+      centered
+    >
+      <Wrapper>
+        <Tabs
+          activeKey={levelSort}
+          type="card"
+          style={{ marginTop: "5px" }}
+          onChange={(idx: string) => handleTabChange(idx)}
+        >
+          {levelList.map((item: any) => (
+            <Tabs.TabPane
+              key={item.sort}
+              tab={item.title}
+              disabled={loading}
+            />
+          ))}
+        </Tabs>
+        <Spin spinning={loading}>
+          <div className="tabs-content">
+            <Transfer
+              targetKeys={targetKeys}
+              selectedKeys={selectedKeys}
+              render={item => item.title}
+              listStyle={{ width: "267px", height: "300px" }}
+              onSelectChange={(keys: string[], keys1: string[]) =>
+                setSelectedKeys(keys.concat(keys1))
               }
-            })}
-            titles={['全部要求', '已选要求']} />
-        </div>
-      </Spin>
-    </Wrapper>
-  </Modal>
+              onChange={(keys: any) => setTargetKeys(keys)}
+              dataSource={promotionList.map((item: any) => {
+                return {
+                  key: item.code,
+                  title: item.name,
+                  description: item.name
+                }
+              })}
+              titles={["全部要求", "已选要求"]}
+            />
+          </div>
+        </Spin>
+      </Wrapper>
+    </Modal>
+  )
 })
 
 const Wrapper = styled.div`
-  .tabs-content{
+  .tabs-content {
     padding-left: 10px;
   }
-  .ant-tabs-nav{
+  .ant-tabs-nav {
     padding-left: 3px;
   }
 `
