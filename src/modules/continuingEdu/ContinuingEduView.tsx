32dc0c6fa0b83a714e1ef8cee1ce82234d298a5c
shiwei <shiwei@cr-health.com>
2020-04-13 08:59:09 +0800

    隐藏题库管理

--- src/modules/continuingEdu/ContinuingEduView.tsx
+++ src/modules/continuingEdu/ContinuingEduView.tsx
@@ -121,7 +121,7 @@ export default function ContinuingEdu(props: Props) {
       icon: <TKGL />,
       path: "/continuingEdu/questionBankManagement",
       component: 题库管理,
-      hide: !appStore.isDev
+      hide: true
     },
     {
       title: "菜单设置",
