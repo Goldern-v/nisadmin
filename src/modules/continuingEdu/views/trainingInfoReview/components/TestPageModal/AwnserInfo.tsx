caa0e9ecd773d911613be988f209af143d00a2ca
wumin <18196569163@163.com>
2020-06-02 09:37:47 +0800

    查看试卷弹窗  右边栏页面优化

--- src/modules/continuingEdu/views/trainingInfoReview/components/TestPageModal/AwnserInfo.tsx
+++ src/modules/continuingEdu/views/trainingInfoReview/components/TestPageModal/AwnserInfo.tsx
@@ -1,102 +1,128 @@
-import styled from 'styled-components'
-import React, { useState, useEffect } from 'react'
-import { Button } from 'antd'
+import styled from "styled-components";
+import React, { useState, useEffect } from "react";
+import { Button } from "antd";
 export interface Props {
-  info?: any
+  info?: any;
 }
 
 export default function answerInfo(props: Props) {
-  const { info } = props
-  const questionList = info.questionList || []
+  const { info } = props;
+  const questionList = info.questionList || [];
 
   //单选题列表
-  let singleChoiceArr = questionList.filter((item: any) => item.questionType == 1)
+  let singleChoiceArr = questionList.filter(
+    (item: any) => item.questionType == 1
+  );
 
   //多选题列表
-  let multiChoiceArr = questionList.filter((item: any) => item.questionType == 2)
+  let multiChoiceArr = questionList.filter(
+    (item: any) => item.questionType == 2
+  );
 
   //填空题
-  let fullArr = questionList.filter((item: any) => item.questionType == 3)
+  let fullArr = questionList.filter((item: any) => item.questionType == 3);
 
   //问答题
-  let wendaArr = questionList.filter((item: any) => item.questionType == 4)
+  let wendaArr = questionList.filter((item: any) => item.questionType == 4);
 
-  return <Wrapper>
-    <div className="main-title">参考答案</div>
-    <div className="title">{info.title}</div>
-    <div>【单选题】</div>
-    <div>{singleChoiceArr.map((item: any) =>
-      <span
-        key={item.sort}>
-        {item.sort}.
-          {(item.answerList || [])
-          .filter((answer: any) => answer.isRight)
-          .map((answer: any) => answer.optionLabel)
-          .join(',')};
-          <span> </span>
-      </span>)}
-    </div>
-    <div>【多选题】</div>
-    <div>{multiChoiceArr.map((item: any) =>
-      <span
-        key={item.sort}>
-        {item.sort}.
-        {(item.answerList || [])
-          .filter((answer: any) => answer.isRight)
-          .map((answer: any) => answer.optionLabel)
-          .join(',')};
-          <span> </span>
-      </span>)}
-    </div>
-    <div>【填空题】</div>
-    <div>
-      {fullArr.map((item: any, idx: number) => <React.Fragment
-        key={idx}>
-        <span>
-          {item.sort}.
-          {item.answer && item.answer.rightAnswer};
-          <span> </span>
-        </span>
-        <br />
-      </React.Fragment>)}
-    </div>
-    <div>【问答题】</div>
-    <div>
-      {wendaArr.map((item: any, idx: number) => <React.Fragment
-        key={idx}>
-        <span>
-          {item.sort}.
-          {item.answer && item.answer.suggestedAnswer};
-          <span> </span>
-        </span>
-        <br />
-      </React.Fragment>)}
-    </div>
-    {info.teachingMethod == 3 && <div className="notice">
-      <div>特别提醒：</div>
-      试卷预览，只是临时性的模拟一次考试试题，不会保存当前你所看到的记录。如果你的题目是
-      <span style={{ color: 'red' }}>随机排序</span>
-      的，可能下次你看到的就会不一样。
-    </div>}
-  </Wrapper>
+  return (
+    <Wrapper>
+      <div className="main-title">参考答案</div>
+      <div className="title">{info.title}</div>
+      {singleChoiceArr && singleChoiceArr.length > 0 && (
+        <div>
+          <div>【单选题】</div>
+          <div>
+            {singleChoiceArr.map((item: any) => (
+              <span key={item.sort}>
+                {item.sort}.
+                {(item.answerList || [])
+                  .filter((answer: any) => answer.isRight)
+                  .map((answer: any) => answer.optionLabel)
+                  .join(",")}
+                ;<span> </span>
+              </span>
+            ))}
+          </div>
+        </div>
+      )}
+      {multiChoiceArr && multiChoiceArr.length > 0 && (
+        <div>
+          <div>【多选题】</div>
+          <div>
+            {multiChoiceArr.map((item: any) => (
+              <span key={item.sort}>
+                {item.sort}.
+                {(item.answerList || [])
+                  .filter((answer: any) => answer.isRight)
+                  .map((answer: any) => answer.optionLabel)
+                  .join(",")}
+                ;<span> </span>
+              </span>
+            ))}
+          </div>
+        </div>
+      )}
+      {fullArr && fullArr.length > 0 && (
+        <div>
+          <div>【填空题】</div>
+          <div>
+            {fullArr.map((item: any, idx: number) => (
+              <React.Fragment key={idx}>
+                <span>
+                  {item.sort}.{item.answer && item.answer.rightAnswer};
+                  <span> </span>
+                </span>
+                <br />
+              </React.Fragment>
+            ))}
+          </div>
+        </div>
+      )}
+      {wendaArr && wendaArr.length > 0 && (
+        <div>
+          <div>【问答题】</div>
+          <div>
+            {wendaArr.map((item: any, idx: number) => (
+              <React.Fragment key={idx}>
+                <span>
+                  {item.sort}.{item.answer && item.answer.suggestedAnswer};
+                  <span> </span>
+                </span>
+                <br />
+              </React.Fragment>
+            ))}
+          </div>
+        </div>
+      )}
+      {info.teachingMethod == 3 && (
+        <div className="notice">
+          <div>特别提醒：</div>
+          试卷预览，只是临时性的模拟一次考试试题，不会保存当前你所看到的记录。如果你的题目是
+          <span style={{ color: "red" }}>随机排序</span>
+          的，可能下次你看到的就会不一样。
+        </div>
+      )}
+    </Wrapper>
+  );
 }
 const Wrapper = styled.div`
   padding: 15px 10px;
-  .main-title{
+  .main-title {
     font-size: 22px;
-    color:#000;
+    color: #000;
     font-weight: bold;
   }
-  .title{
+  .title {
     margin-top: 18px;
     margin-bottom: 10px;
     font-size: 16px;
-    color:#000;
+    color: #000;
     font-weight: bold;
   }
-  .notice{
+  .notice {
     margin-top: 15px;
     padding: 15px;
     background: #eee;
   }
-`
+`;
