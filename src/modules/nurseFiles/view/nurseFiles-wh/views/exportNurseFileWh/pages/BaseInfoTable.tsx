6f6de3870302d45098b17492a3ba3ba824f40c17
shiwei <shiwei@cr-health.com>
2020-10-10 16:05:52 +0800

    武汉导出档案提交

---
+++ src/modules/nurseFiles/view/nurseFiles-wh/views/exportNurseFileWh/pages/BaseInfoTable.tsx
@@ -0,0 +1,80 @@
+import styled from 'styled-components'
+import React, { useState, useEffect } from 'react'
+// import { Button } from 'antd'
+import { TableCon } from './../components/TableCon'
+
+export interface Props {
+  baseInfo?: any
+}
+
+export default function BaseInfoTable(props: Props) {
+  const { baseInfo } = props
+
+  return <Wrapper>
+    <colgroup>
+      <col />
+    </colgroup>
+    <tbody>
+      <tr className="main-title-row">
+        <td colSpan={5}>基本情况</td>
+      </tr>
+      <tr>
+        <td>姓名</td>
+        <td>{baseInfo.empName}</td>
+        <td>性别</td>
+        <td>{baseInfo.sex}</td>
+        <td rowSpan={5}>
+          {baseInfo.nearImageUrl ? (
+            <img
+              src={baseInfo.nearImageUrl}
+              alt='' className='nearImage' />
+          ) : '近期免冠照片'}
+        </td>
+      </tr>
+      <tr>
+        <td>出生年月</td>
+        <td>{baseInfo.nurseHierarchyDate}</td>
+        <td>民族</td>
+        <td>{baseInfo.nation}</td>
+      </tr>
+      <tr>
+        <td>籍贯</td>
+        <td>{baseInfo.nativePlace}</td>
+        <td>职务</td>
+        <td>{baseInfo.job}</td>
+      </tr>
+      <tr>
+        <td>参加工作时间</td>
+        <td>{baseInfo.takeWorkTime}</td>
+        <td>来院参加工作时间</td>
+        <td>{baseInfo.goHospitalWorkDate}</td>
+      </tr>
+      <tr>
+        <td>技术职称</td>
+        <td>{baseInfo.newTitle}</td>
+        <td>最高学历</td>
+        <td>{baseInfo.highestEducation}</td>
+      </tr>
+      <tr>
+        <td>身份证号码</td>
+        <td colSpan={2}>{baseInfo.cardNumber}</td>
+        <td>政治面貌</td>
+        <td>{baseInfo.politicsLook}</td>
+      </tr>
+      <tr>
+        <td>护士执业证书编号</td>
+        <td colSpan={2}>{baseInfo.zyzsNumber}</td>
+        <td>联系电话</td>
+        <td>{baseInfo.phone}</td>
+      </tr>
+    </tbody>
+  </Wrapper>
+}
+const Wrapper = styled(TableCon)`
+  img{
+    width: 130px;
+    margin: auto;
+    display: block;
+    object-fit: contain;
+  }
+`
--- src/modules/nurseFiles/view/nurseFiles-wh/views/nurseFileDetail/components/TopCon.tsx
+++ src/modules/nurseFiles/view/nurseFiles-wh/views/nurseFileDetail/components/TopCon.tsx
@@ -11,7 +11,9 @@ import LeaveModal from '../modal/LeaveModal'
 import qs from 'qs'
 import { nurseFilesService } from '../../../services/NurseFilesService'
 import { isSelf } from '../views/BaseInfo'
-export interface Props extends RouteComponentProps {}
+import ExportNurseFileWh from './../../exportNurseFileWh/ExportNurseFileWh'
+
+export interface Props extends RouteComponentProps { }
 
 const BG = require('../../../images/顶部背景.png')
 
@@ -22,6 +24,7 @@ const WARNNING_ICON = require('../../../images/注意.png')
 export default observer(function TopCon() {
   const deptChangeModal = createModal(DeptChangeModal)
   const leaveModal = createModal(LeaveModal)
+  const [exportVisible, setExportVisible] = useState(false)
   let history = store.appStore.history
   let { empName, post, deptName, nurseHierarchy, nearImageUrl } = nurseFileDetailViewModal.nurserInfo
   const openLeaveModalModal = () => {
@@ -82,12 +85,13 @@ export default observer(function TopCon() {
             </span>
           </Tip>
         ) : (
-          <Tip />
-          // <Tip>你没有待审核的信息</Tip>
-        )}
+            <Tip />
+            // <Tip>你没有待审核的信息</Tip>
+          )}
       </Name>
       {authStore.isRoleManage && !isSelf() && (
         <React.Fragment>
+          <ExportBtn onClick={() => setExportVisible(true)}>导出档案</ExportBtn>
           <DeptChangeBtn1 onClick={() => openLeaveModalModal()}>离职/退休</DeptChangeBtn1>
           <DeptChangeBtn onClick={() => openDeptChangeModal()}>片区内调动</DeptChangeBtn>
         </React.Fragment>
@@ -95,6 +99,12 @@ export default observer(function TopCon() {
 
       <deptChangeModal.Component title='片区内调动' />
       <leaveModal.Component title='离职/退休' />
+      {/* 档案导出 */}
+      {exportVisible && (
+        <ExportNurseFileWh
+          empNo={appStore.queryObj.empNo}
+          callback={() => { setExportVisible(false) }} />
+      )}
     </Wrapper>
   )
 })
@@ -161,6 +171,12 @@ const ClickSpan = styled.span`
   margin: 0px 4px;
   display: inline-block;
   border-bottom: 1px solid #5472c4;
+`
+
+const ExportBtn = styled(Button)`
+  position: absolute !important;
+  right: 218px;
+  top: 34px;
 `
 
 const DeptChangeBtn = styled(Button)`
