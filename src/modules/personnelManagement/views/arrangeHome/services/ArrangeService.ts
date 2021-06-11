ceb17ebad01a9ba44d3844f608f91c597e7af12b
Dingsd <1162620357@qq.com>
2021-06-11 16:42:38 +0800

    feat(江门妇幼-排班)：排班导出科室接口替换

--- src/modules/personnelManagement/views/arrangeHome/services/ArrangeService.ts
+++ src/modules/personnelManagement/views/arrangeHome/services/ArrangeService.ts
@@ -117,7 +117,13 @@ export default class ArrangeService extends BaseApiService {
       startTime: selectViewModal.params.startTime, // stratTime 开始时间（刚开始由后台传给前台）
       endTime: selectViewModal.params.endTime // endTime   结束时间（刚开始由后台传给前台）
     };
-    const url = appStore.HOSPITAL_ID === 'lcey' ? 'schedulingLc/export' : '/scheduling/export'
+    const url = appStore.hisMatch({
+      map: {
+        lcey: '/schedulingLc/export',
+        jmfy: '/schedulingJm/export',
+        default: '/scheduling/export',
+      }
+    })
     return this.post(url, postData, { responseType: "blob" });
   }
 
