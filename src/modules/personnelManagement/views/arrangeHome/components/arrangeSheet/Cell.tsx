d794313686ac831cf822d52bb5c1414a95ac9c7c
Dingsd <1162620357@qq.com>
2021-06-17 18:45:47 +0800

    feat(东莞横沥)：排班加减班问题修复

--- src/modules/personnelManagement/views/arrangeHome/components/arrangeSheet/Cell.tsx
+++ src/modules/personnelManagement/views/arrangeHome/components/arrangeSheet/Cell.tsx
@@ -95,7 +95,7 @@ export default observer(function Cell(props: Props) {
         {
           type: "line"
         },
-        ['wh', 'nys', 'jmfy', 'lcey'].includes(appStore.HOSPITAL_ID)
+        ['wh', 'nys', 'jmfy', 'lcey', 'dghl'].includes(appStore.HOSPITAL_ID)
           ? {
             icon: require("../../images/修改工时.png"),
             label: "加/减班",
@@ -507,6 +507,31 @@ export default observer(function Cell(props: Props) {
       );
     },
     lcey: () => {
+      return (
+        (cellObj.schAddOrSubs &&
+        cellObj.schAddOrSubs.length &&
+        cellObj.schAddOrSubs[0].statusType == "1"
+          ? "加班"
+          : "减班") +
+        ":" +
+        (cellObj.schAddOrSubs && cellObj.schAddOrSubs.length
+          ? cellObj.schAddOrSubs[0].hour
+          : 0) +
+        "h" +
+        "，" +
+        `现:${cellObj.effectiveTime || 0}h，` +
+        `原:${cellObj.effectiveTimeOld || 0}h，` +
+        `白:${(cellObj.schAddOrSubs &&
+          cellObj.schAddOrSubs.length &&
+          cellObj.schAddOrSubs[0].settingMorningHour) ||
+        0}h，` +
+        `夜:${(cellObj.schAddOrSubs &&
+          cellObj.schAddOrSubs.length &&
+          cellObj.schAddOrSubs[0].settingNightHour) ||
+        0}h`
+      );
+    },
+    dghl: () => {
       return (
         (cellObj.schAddOrSubs &&
         cellObj.schAddOrSubs.length &&
