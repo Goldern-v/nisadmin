8b3eaf1f02e9c165b53845cad1b75b24eb483e8e
hsenap <1244696102@qq.com>
2021-11-12 14:26:47 +0800

    [武汉亚心]审核管理、护理排班、档案管理替换为武汉市一

--- src/layouts/components/navConfig_whyx.tsx
+++ src/layouts/components/navConfig_whyx.tsx
@@ -43,41 +43,41 @@ const baseConfig: navConfigItem[] = [
   //   name: '不良事件分析报告',
   //   path: '/badEvents/alanysis/1/1'
   // },
-  {
-    name: "质量管理",
-    children: [
-      {
-        name: "三级质量",
-        path: "/qcThree",
-        icon: require("../images/menu-icon/三级质控@2x.png")
-      },
-      {
-        name: "二级质量",
-        path: "/qcTwo",
-        icon: require("../images/menu-icon/二级质控@2x.png")
-      },
-      {
-        name: "一级质量",
-        path: "/qcOneHj",
-        icon: require("../images/menu-icon/一级质控@2x.png")
-      },
-      // {
-      //   name: '病区登记本',
-      //   path: '',
-      //   icon: require('../images/menu-icon/病区登记本@2x.png')
-      // },
-      {
-        name: "查询统计",
-        path: "/queryStatistics",
-        icon: require("../images/menu-icon/护理查房@2x.png")
-      },
-      {
-        name: "护理查房",
-        path: "/checkWard",
-        icon: require("../images/menu-icon/护理查房@2x.png")
-      }
-    ]
-  },
+  // {
+  //   name: "质量管理",
+  //   children: [
+  //     {
+  //       name: "三级质量",
+  //       path: "/qcThree",
+  //       icon: require("../images/menu-icon/三级质控@2x.png")
+  //     },
+  //     {
+  //       name: "二级质量",
+  //       path: "/qcTwo",
+  //       icon: require("../images/menu-icon/二级质控@2x.png")
+  //     },
+  //     {
+  //       name: "一级质量",
+  //       path: "/qcOneHj",
+  //       icon: require("../images/menu-icon/一级质控@2x.png")
+  //     },
+  //     {
+  //       name: '病区登记本',
+  //       path: '',
+  //       icon: require('../images/menu-icon/病区登记本@2x.png')
+  //     },
+  //     {
+  //       name: "查询统计",
+  //       path: "/queryStatistics",
+  //       icon: require("../images/menu-icon/护理查房@2x.png")
+  //     },
+  //     {
+  //       name: "护理查房",
+  //       path: "/checkWard",
+  //       icon: require("../images/menu-icon/护理查房@2x.png")
+  //     }
+  //   ]
+  // },
   {
     name: "学习培训",
     path: "/continuingEdu"
@@ -114,7 +114,11 @@ const baseConfig: navConfigItem[] = [
     path: "/setting"
   },
   {
-    name: "护理人员管理",
+    name: "我的档案",
+    path: "/selfNurseFile"
+  },
+  {
+    name: "排班管理",
     path: "/personnelManagement"
   }
 ];
--- src/modules/personnelManagement/views/arrangeHome/components/SelectCon.tsx
+++ src/modules/personnelManagement/views/arrangeHome/components/SelectCon.tsx
@@ -490,7 +490,7 @@ export default observer(function SelectCon() {
               <span onClick={() => handleExport()}>批量导出</span>
             </div>
           )}
-        {(["wh", "gxjb", "fsxt"].includes(appStore.HOSPITAL_ID)) &&
+        {(["wh", "gxjb", "fsxt", "whyx"].includes(appStore.HOSPITAL_ID)) &&
           (authStore.isDepartment || authStore.isSupervisorNurse) && (
             <div className="item">
               <Dropdown.Button
@@ -501,7 +501,7 @@ export default observer(function SelectCon() {
               </Dropdown.Button>
             </div>
           )}
-        {(["wh", "gxjb", "fsxt"].includes(appStore.HOSPITAL_ID)) && (
+        {(["wh", "gxjb", "fsxt", "whyx"].includes(appStore.HOSPITAL_ID)) && (
           <div className="item">
             <Button
               className="item"
