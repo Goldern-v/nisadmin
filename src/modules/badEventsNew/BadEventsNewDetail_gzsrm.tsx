44407600f29e35b79bc521cb1aef60b5d2ce74e9
hsenap <1244696102@qq.com>
2021-12-09 09:30:48 +0800

    [贵州]不良事件审核调整

--- src/modules/badEventsNew/BadEventsNewDetail_gzsrm.tsx
+++ src/modules/badEventsNew/BadEventsNewDetail_gzsrm.tsx
@@ -164,7 +164,7 @@ export default withRouter(function BadEventsNewDetail(props: any) {
     if (!authStore.user) return ''
     if (Object.keys(stepNext).length <= 0) return ''
     if (['commit', 'save'].includes(stepNext?.nodeCode)) return ''
-
+    if(itemDataMap.B0002061 && itemDataMap.B0002061 == '2') return ''//非护理不良事件不返回
     let btnText = stepNext.nodeName
 
     if (stepNext?.canHandle) btnDisable = false
