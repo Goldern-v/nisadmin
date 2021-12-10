1775d26680a79f2c3c4be045864155519b0d999f
wyj <wen_yongjie@126.com>
2021-12-10 14:12:57 +0800

    fix:【贵州】不良事件审核流程调整

--- src/modules/badEventsNew/BadEventsNewDetail_gzsrm.tsx
+++ src/modules/badEventsNew/BadEventsNewDetail_gzsrm.tsx
@@ -79,6 +79,7 @@ export default withRouter(function BadEventsNewDetail(props: any) {
     // let devFormUrl = 'http://localhost:8088'
     let devFormUrl = `${protocol}//${hostname}:${port}${formUrl}`
     let commonUrl = appStore.isDev ? devFormUrl : formUrl
+    console.log(appStore.isDev, commonUrl)
     return `${commonUrl}/不良事件病人安全通报单.html?${qs.stringify(query)}`
   }
 
@@ -164,11 +165,11 @@ export default withRouter(function BadEventsNewDetail(props: any) {
     let btnDisable = iframeLoading
     if (!authStore.user) return ''
     if (Object.keys(stepNext).length <= 0) return ''
-    if (['commit', 'save'].includes(stepNext?.nodeCode)) return ''
+    if (['commit', 'save'].includes(stepNext ?.nodeCode)) return ''
     if (itemDataMap.B0002061 && itemDataMap.B0002061 == '2') return ''//非护理不良事件不返回
     let btnText = stepNext.nodeName
 
-    if (stepNext?.canHandle) btnDisable = false
+    if (stepNext ?.canHandle) btnDisable = false
 
     return (
       <Button
@@ -240,7 +241,7 @@ export default withRouter(function BadEventsNewDetail(props: any) {
             打印
           </Button> */}
         </div>
-        <div className='status'>状态：{stepCurrent?.nodeName}</div>
+        <div className='status'>状态：{stepCurrent ?.nodeName}</div>
       </div>
       <div className='main-contain'>
         <div className='status-line'>
