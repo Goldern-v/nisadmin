be8e3042588eee60abde9b2a5e2bfd625bc977e7
hgy <1309843131@qq.com>
2021-12-10 14:24:00 +0800

    【聊城】打开不良事件

--- src/layouts/components/navConfig_lcey.tsx
+++ src/layouts/components/navConfig_lcey.tsx
@@ -14,11 +14,11 @@ export interface navConfigItem {
 const baseConfig: navConfigItem[] = [
   {
     name: "首页",
-    path: "/home"
+    path: "/home",
   },
   {
     name: "审核管理",
-    path: "/auditsManagement"
+    path: "/auditsManagement",
   },
   // {
   //   name: "病区日志",
@@ -27,13 +27,13 @@ const baseConfig: navConfigItem[] = [
   // },
   {
     name: "档案管理",
-    path: "/nurseFile"
+    path: "/nurseFile",
   },
-  // {
-  //   name: '不良事件',
-  //   path: '/badEventsNewList',
-  //   hidden: !appStore.isDev,
-  // },
+  {
+    name: "不良事件",
+    path: "/badEventsNew",
+    // hidden: !appStore.isDev,
+  },
   // {
   //   name: '不良事件分析报告',
   //   path: '/badEvents/alanysis/1/1'
@@ -44,17 +44,17 @@ const baseConfig: navConfigItem[] = [
       {
         name: "三级质量",
         path: "/qcThree",
-        icon: require("../images/menu-icon/三级质控@2x.png")
+        icon: require("../images/menu-icon/三级质控@2x.png"),
       },
       {
         name: "二级质量",
         path: "/qcTwo",
-        icon: require("../images/menu-icon/二级质控@2x.png")
+        icon: require("../images/menu-icon/二级质控@2x.png"),
       },
       {
         name: "一级质量",
         path: "/qcOneHj",
-        icon: require("../images/menu-icon/一级质控@2x.png")
+        icon: require("../images/menu-icon/一级质控@2x.png"),
       },
       // {
       //   name: '病区登记本',
@@ -64,23 +64,23 @@ const baseConfig: navConfigItem[] = [
       {
         name: "查询统计",
         path: "/queryStatistics",
-        icon: require("../images/menu-icon/护理查房@2x.png")
+        icon: require("../images/menu-icon/护理查房@2x.png"),
       },
       {
         name: "护理查房",
         path: "/checkWard",
-        icon: require("../images/menu-icon/护理查房@2x.png")
-      }
-    ]
+        icon: require("../images/menu-icon/护理查房@2x.png"),
+      },
+    ],
   },
   {
     name: "学习培训",
-    path: "/continuingEdu"
+    path: "/continuingEdu",
     // hidden: !appStore.isDev
   },
   {
     name: "敏感指标",
-    path: "/indicator"
+    path: "/indicator",
   },
   {
     name: "敏感指标登记本",
@@ -89,26 +89,28 @@ const baseConfig: navConfigItem[] = [
   },
   {
     name: "统计查询",
-    path: "/statistic"
+    path: "/statistic",
   },
   {
     name: "通知公告",
-    path: "/notice"
+    path: "/notice",
   },
   {
     name: "护理制度",
-    path: "/nursingRulesNew"
+    path: "/nursingRulesNew",
   },
   {
     name: "系统设置",
-    path: "/setting"
+    path: "/setting",
   },
   {
     name: "护理排班",
-    path: "/personnelManagement"
-  }
+    path: "/personnelManagement",
+  },
 ];
 
-const beConfig: navConfigItem[] = []
+const beConfig: navConfigItem[] = [];
 
-export const navConfig: navConfigItem[] = appStore.onlyBadEvent ? beConfig : baseConfig
+export const navConfig: navConfigItem[] = appStore.onlyBadEvent
+  ? beConfig
+  : baseConfig;
