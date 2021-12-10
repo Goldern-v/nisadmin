bd635cf75463e4f84a99ca117b4c36f22fb22276
pgm <1796621138@qq.com>
2021-12-10 11:30:06 +0800

    fix: jmfy-新需求-产假及哺乳期归档

--- src/modules/personnelManagement/views/arrangeHome/page/maternityLeave/justice.tsx
+++ src/modules/personnelManagement/views/arrangeHome/page/maternityLeave/justice.tsx
@@ -7,11 +7,10 @@ import { Button, message } from "antd"
 import styled from "styled-components"
 import DeptSelect from "src/components/DeptSelect"
 import BaseTable from "src/components/BaseTable"
-import EditModal from './components/editModal'
+// import EditModal from './components/editModal'
 import { SearchForm, iModalForm } from './modal'
 import config from './config'
 import api from './api'
-import moment from "moment";
 
 export default observer(() => {
   const [pageLoading, setPageLoading] = useState(false)
@@ -21,24 +20,10 @@ export default observer(() => {
   }
   const [tableData, setTableData] = useState([])
   const [total, setTotal] = useState(0)
-  const [modalId, setModalId] = useState('')
-  const [modalVisible, setModalVisible] = useState(false)
+  // const [modalId, setModalId] = useState('')
+  // const [modalVisible, setModalVisible] = useState(false)
 
-  const columns = config.creatColumns((type: string, item: iModalForm) => {
-    switch (type) {
-      case 'edit':
-        setModalId(item.id || '')
-        setModalVisible(true)
-        break
-      case 'delete':
-        globalModal.confirm("确认删除", "确认删除该套餐？").then(async res => {
-          await api.deleteItem(item.id || '')
-          message.success(`删除成功`)
-          await getData()
-        });
-        break
-    }
-  })
+  const columns = config.creatJusticeColumns()
 
   const getData = async () => {
     if (!form.deptCode) return
@@ -56,18 +41,18 @@ export default observer(() => {
     setTableData(data.list)
   }
 
-  const handleCreate = () => {
-    setModalId('')
-    setModalVisible(true)
-  }
+  // const handleCreate = () => {
+  //   setModalId('')
+  //   setModalVisible(true)
+  // }
 
-  const handleModalUpdate = async () => {
-    setModalVisible(false)
-    await getData()
-  }
+  // const handleModalUpdate = async () => {
+  //   setModalVisible(false)
+  //   await getData()
+  // }
 
-  const exportData = () => {
-  }
+  // const exportData = () => {
+  // }
 
   useEffect(() => {
     const promise = getData()
@@ -87,18 +72,18 @@ export default observer(() => {
         <span className="label">科室</span>
         <DeptSelect hasAllDept onChange={deptCode => setFormItem({ 'deptCode': deptCode })} />
         {/* <span className="label">分娩日期</span> */}
-        <span className="label">选择时间</span>
+        <span className="label">查询时间</span>
         <DatePicker.RangePicker
           value={form.date}
           onChange={(dates) => setFormItem({ 'date': dates })} />
-        <span className="label">申请状态</span>
+        {/* <span className="label">申请状态</span>
         <Select value={form.status} onChange={(value: string) => setFormItem({ 'status': value })}>
           {config.statusOption.map((item, index) => (
             <Select.Option value={item.value} key={index}>
               {item.label}
             </Select.Option>
           ))}
-        </Select>
+        </Select> */}
         <span className="label">分娩方式</span>
         <Select value={form.deliveryMode} onChange={(value: string) => setFormItem({ 'deliveryMode': value })}>
           <Select.Option value={''}>全部</Select.Option>
@@ -111,7 +96,7 @@ export default observer(() => {
 
         <Button onClick={() => getData()}>查询</Button>
         {/*<Button onClick={() => exportData()}>导出</Button>*/}
-        <Button type="primary" onClick={handleCreate}>新增</Button>
+        {/* <Button type="primary" onClick={handleCreate}>新增</Button> */}
       </PageHeader>
       {/* 表格 */}
       <BaseTable
@@ -133,14 +118,14 @@ export default observer(() => {
         }}
       />
       {/* 新增修改弹窗 */}
-      {
+      {/* {
         modalVisible && <EditModal
           modalId={modalId}
           visible={modalVisible}
           onOk={handleModalUpdate}
           onCancel={() => setModalVisible(false)}
         />
-      }
+      } */}
 
     </Wrapper>
   )
