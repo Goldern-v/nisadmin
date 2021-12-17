9cee4588f1198c4f30d7cefbdab1e19f5216545d
hgy <1309843131@qq.com>
2021-12-17 09:17:20 +0800

    【贵州】三级质控增加质量统计

---
+++ src/modules/quality/views/qcFormGzsrm/三级质控护理质量统计汇总.tsx
@@ -0,0 +1,9 @@
+import { observer } from "mobx-react";
+import React from "react";
+import styled from "styled-components";
+
+export default observer(function(props) {
+  return <Wrapper />;
+});
+
+const Wrapper = styled.div``;
