4432cfd0375736022cac9b24d4e5757fd3daad50
wyj <wen_yongjie@126.com>
2021-12-27 14:13:37 +0800

    feat:【福清】开放不良事件入口

--- src/modules/badEventsNew/BadEventsRouters.tsx
+++ src/modules/badEventsNew/BadEventsRouters.tsx
@@ -150,13 +150,13 @@ export default function BadEventsRouters(props: Props) {
     redictUri: '/crNursing/badEvent',
     loginUri: '/crNursing/login'
   }
-  if (['fqfybjy'].includes(appStore.HOSPITAL_ID)) { //福清特殊跳转单独的上报系统
-    autoLoginInfo = {
-      blank: true,
-      redictUri: '/crNursing/badevents/index',
-      loginUri: '/crNursing/badevents/login'
-    }
-  }
+  // if (['fqfybjy'].includes(appStore.HOSPITAL_ID)) { //福清特殊跳转单独的上报系统
+  //   autoLoginInfo = {
+  //     blank: true,
+  //     redictUri: '/crNursing/badevents/index',
+  //     loginUri: '/crNursing/badevents/login'
+  //   }
+  // }
   return (
     <Wrapper>
       <LeftMenuCon>
