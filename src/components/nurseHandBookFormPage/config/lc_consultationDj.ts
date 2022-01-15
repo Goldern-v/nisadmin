92eb3cbfef85a88cd4aa903e7e3969d4193f21c5
TXY <2609813922@qq.com>
2022-01-15 16:31:38 +0800

    1

--- src/components/nurseHandBookFormPage/config/lc_consultationDj.ts
+++ src/components/nurseHandBookFormPage/config/lc_consultationDj.ts
@@ -10,12 +10,12 @@ const tHead = {
   top: [
     { name: "日期/时间", colspan: "1", rowspan: "1", style:{ width: 150 } },
     { name: "患者姓名", colspan: "1", rowspan: "1", style:{ width: 100 } },
-    { name: "病案号", colspan: "1", rowspan: "1", style:{ width: 100 } },
+    // { name: "病案号", colspan: "1", rowspan: "1", style:{ width: 100 } },
     { name: "主要诊断", colspan: "1", rowspan: "1", style:{ width: 100 } },
-    { name: "会诊类型", colspan: "1", rowspan: "1", style:{ width: 100 } },
+    // { name: "会诊类型", colspan: "1", rowspan: "1", style:{ width: 100 } },
     { name: "受邀科室", colspan: "1", rowspan: "1", style:{ width: 100 } },
-    { name: "会诊人员", colspan: "1", rowspan: "1", style:{ width: 125 } },
-    { name: "申请时间", colspan: "1", rowspan: "1", style:{ width: 125 } },
+    { name: "会诊人员", colspan: "1", rowspan: "1", style:{ width: 150 } },
+    { name: "申请时间", colspan: "1", rowspan: "1", style:{ width: 150 } },
     { name: "会诊时间", colspan: "1", rowspan: "1", style:{ width: 150 } },
   ],
   mid: [],
@@ -25,18 +25,18 @@ const tHead = {
 const tBody: any = [
   { key: "time", name: "日期/时间", value: "", width: 150, click: timePicker },
   { key: "patientName", name: "患者姓名", value: "", width: 100 },
-  { key: "recordId", name: "病案号", value: "", width: 100 },
+  // { key: "recordId", name: "病案号", value: "", width: 100 },
   { key: "diagnosis", name: "主要诊断", value: "", width: 100 },
-  { key: "type", name: "会诊类型", value: "", width: 100 },
+  // { key: "type", name: "会诊类型", value: "", width: 100 },
   { key: "deptName", name: "受邀科室", value: "", width: 100 },
-  { key: "personnel", name: "会诊人员", value: "", width: 125 },
-  { key: "applyTime", name: "申请时间", value: "", width: 125 },
+  { key: "personnel", name: "会诊人员", value: "", width: 150 },
+  { key: "applyTime", name: "申请时间", value: "", width: 150 },
   { key: "consultationTime", name: "会诊时间", value: "", width: 150 },
 ]
 
 export default {
   defaulLength: [20],
-  tableTitle: { value: `${user.deptName}护理会诊登记表`, width: 1042 },
+  tableTitle: { value: `${user.deptName}护理会诊登记表`, width: 894 },
   tBody: [tBody],
   tHead: [tHead],
 }
