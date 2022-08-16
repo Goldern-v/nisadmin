ca7c606298bd8cd32dfb98147d59fc94212cccd2
goldern <dingjunwei@dingjunweideMacBook-Pro.local>
2022-08-16 18:43:42 +0800

    [档案]:打印带上附件图片

---
+++ src/modules/nurseFiles/view/nurseFiles-hj/views/exportNurseFile/page/ImgPrint.tsx
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+import React, { useState, useEffect } from "react";
+import { Button } from "antd";
+import PrintPage from "../components/PrintPage";
+import { appStore } from "src/stores";
+
+export interface Props {
+  imgObj: {
+    filterData:any[],
+    content:""
+  },
+  startPage:any[],
+  startIndex:0
+}
+
+export default function OnePage(props: Props) {
+  const { imgObj,startPage,startIndex } = props;
+  const showImage = imgObj.filterData.map((item:any,index:any)=>{
+    return (
+      <PrintPage key={index} pageIndex={startPage[startIndex]+index}>
+        <div className="NavTlt">附件</div>
+        <div className="title">{imgObj.content}{imgObj.filterData.length>1&&index+1}</div>
+        <img src="http://120.24.240.231:19091/crNursing/asset/nurseAttachment/20190719/20190719121332sqJFFsFQ.jpg"/>
+      </PrintPage>
+    )
+  })
+  return (
+    <Wrapper>
+      {showImage}
+    </Wrapper>
+  );
+}
+const Wrapper = styled.div`
+  .NavTlt{
+    font-size: 32px;
+    padding: 15px 40px;
+    font-weight: bold; 
+    font-family: "黑体" !important;
+  }
+  .title{
+    font-size: 30px;
+    text-align: center;
+    padding: 0 100px 15px;
+    font-weight: bold;
+    font-family: "黑体" !important;
+  }
+  img{
+    width:80%;
+    max-width:80%;
+    position:relative;
+    left:50%;
+    transform: translateX(-50%);
+  }
+`;
