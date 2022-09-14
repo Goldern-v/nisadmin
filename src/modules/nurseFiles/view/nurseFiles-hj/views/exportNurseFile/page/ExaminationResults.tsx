fdaab5d7d9477c862c18e51b5bfe387688ddc132
Else <15102082382@163.com>
2022-09-14 17:12:00 +0800

    fix:厚街三甲复试表格样式调整

--- src/modules/nurseFiles/view/nurseFiles-hj/views/exportNurseFile/page/ExaminationResults.tsx
+++ src/modules/nurseFiles/view/nurseFiles-hj/views/exportNurseFile/page/ExaminationResults.tsx
@@ -53,7 +53,6 @@ export default function ExaminationResults(props: Props) {
               <td />
               <td />
               <td />
-              <td />
             </tr>
           ))}
         </tbody>
