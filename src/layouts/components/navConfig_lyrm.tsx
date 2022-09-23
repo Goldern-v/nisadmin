b99aa352398611d6d5418ecf22f8a9f0413221fa
twait <1281773314@qq.com>
2022-09-23 11:40:36 +0800

    feat：临邑县人医敏感指标指定地址

--- src/layouts/components/navConfig_lyrm.tsx
+++ src/layouts/components/navConfig_lyrm.tsx
@@ -80,7 +80,7 @@ export const navConfig: (appStore:any,authStore?:any) => navConfigItem[] = (appS
     name: "敏感指标",
     onClick: () => {
       let [http, host, port] = location.origin.split(':');
-      let url = `${location.origin}/bcyNursingQuality/ssoLogin?token=${authStore.authToken}`
+      let url = `http://192.168.4.175:9091/bcyNursingQuality/ssoLogin?token=${authStore.authToken}`
       window.open(url)
     }
   },
