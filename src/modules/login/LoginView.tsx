b31f97bb56dc797b2279160219353df30aa4479f
hgy <1309843131@qq.com>
2022-10-11 14:15:44 +0800

    fix: 贵州-登录增加校验

--- src/modules/login/LoginView.tsx
+++ src/modules/login/LoginView.tsx
@@ -1,8 +1,7 @@
-// import * as React from 'react'
 import React, { useState, useEffect, useRef, useLayoutEffect } from "react";
 import styled from "styled-components";
 import { RouteComponentProps } from "react-router";
-import loginViewModel from "./LoginViewModel";
+// import loginViewModel from "./LoginViewModel";
 
 import service from "src/services/api";
 import { appStore, scheduleStore } from "src/stores";
@@ -32,12 +31,14 @@ export default withRouter(function LoginView(props: Props) {
   const [isSavePassword, setIsSavePassword] = useState(false);
   let userRef: any = useRef<HTMLInputElement>();
   let passwordRef: any = useRef<HTMLInputElement>();
+  // 密码正则
+  const [reg, setReg]: any = useState();
 
   useEffect(() => {
     if (localStorage.lastLoginUserName) {
       setUsername(localStorage.lastLoginUserName);
     }
-
+    setHospitalReg()
     autoLogin()
   }, []);
 
@@ -83,6 +84,11 @@ export default withRouter(function LoginView(props: Props) {
     if (showVerification && !verificationCode) {
       message.warning("请填写验证码！")
       return;
+    }
+    if (reg?.flag) {
+      const pattern = eval(`/${reg?.rule}/`)
+      const flag = pattern.test(_password);
+      if (!flag) return message.warning(reg.ruleMsg)
     }
     (!appStore.isDev) && (["fssdy", "sdlj","dghl"].includes(appStore.HOSPITAL_ID)) && (_password = md5(_password));
     service.authApiService
@@ -182,6 +188,20 @@ export default withRouter(function LoginView(props: Props) {
     let titlePrefix = appStore.hospitalOtherName ? appStore.hospitalOtherName : "";
     return `${titlePrefix}护理管理系统`
   }
+  //获取正则
+  const getPasswordRule = () => {
+    service.authApiService.passwordRule().then((res: any) => {
+      if (res.code == 200) {
+        setReg(res.data)
+      }
+    })
+  }
+  // 设置正则规则
+  const setHospitalReg = () => {
+    if (appStore.HOSPITAL_ID === 'gzsrm') {
+      getPasswordRule()
+    }
+  }
 
   return (
     <Wrapper
@@ -269,7 +289,7 @@ export default withRouter(function LoginView(props: Props) {
             </div>
             {appStore.hisMatch({
               map: {
-                "whyx,sdlj,dghl": <div className="resetpassword CheckItem" onClick={() => {
+                "whyx,sdlj,dghl,gzsrm": <div className="resetpassword CheckItem" onClick={() => {
                   history.push('/resetpassword')
                 }}>
                   <span>重置密码</span>
