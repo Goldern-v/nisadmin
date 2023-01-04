906819617d417fed3097f37fb47df34bfb429430
liuqingbo <liuqingbo@etongeis.com>
2023-01-04 09:49:57 +0800

    南方中西医-新增满意度调查模块

--- src/layouts/components/navConfig_nfzxy.tsx
+++ src/layouts/components/navConfig_nfzxy.tsx
@@ -74,6 +74,11 @@ const baseConfig: navConfigItem[] = [
         name: "护理查房",
         path: "/checkWard",
         icon: require("../images/menu-icon/护理查房@2x.png")
+      },
+      {
+        name: "护士对护理工作满意度调查表",
+        path: "/nurseSatisfactionSurvey",
+        icon: require("../images/menu-icon/护理查房@2x.png"),
       }
     ]
   },
