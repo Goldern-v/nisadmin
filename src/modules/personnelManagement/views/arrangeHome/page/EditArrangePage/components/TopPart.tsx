bfc2f03ec993f4ecc405e5319f1e6dc0eaa7cea2
liuqingbo <liuqingbo@etongeis.com>
2023-01-06 15:37:38 +0800

    武汉亚心，武汉汉口 排班管理 由原来的5号锁定上月排班改为每月7号锁定上月排班

--- src/modules/personnelManagement/views/arrangeHome/page/EditArrangePage/components/TopPart.tsx
+++ src/modules/personnelManagement/views/arrangeHome/page/EditArrangePage/components/TopPart.tsx
@@ -160,7 +160,7 @@ export default observer(function TopPart() {
     //当前月份的时间戳1号
     let todayOne = new Date(getCurrentMonth()[0].format("YYYY-MM-DD")).getTime()
     // 当前月份的时间戳5号
-    let today = moment(moment().format('YYYY-MM-05')).endOf('day').valueOf();
+    let today = moment(moment().format('YYYY-MM-07')).endOf('day').valueOf();
     let currentTime = new Date().getTime()
 
     // 选择开始的时间戳
