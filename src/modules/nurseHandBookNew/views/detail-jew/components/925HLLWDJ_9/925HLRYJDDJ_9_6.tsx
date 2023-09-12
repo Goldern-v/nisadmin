724cba5e7cf3796a4540dd3a0cdf9870f4850cf5
Mr.Zeng <2041824311@qq.com>
2023-09-12 17:29:26 +0800

    【九二五】护长手册内容添加

---
+++ src/modules/nurseHandBookNew/views/detail-jew/components/925HLLWDJ_9/925HLRYJDDJ_9_6.tsx
@@ -0,0 +1,115 @@
+import React from 'react'
+import styled from 'styled-components'
+import { nurseHandbookRecordModel as model } from '../../model'
+import { Button, Input,message} from 'antd'
+import { observer } from 'mobx-react'
+import { DetailCtxCon } from 'src/modules/nurseHandBookNew/style'
+import { Obj } from 'src/libs/types'
+import cloneDeep from 'lodash/cloneDeep'
+import { globalModal } from "src/global/globalModal"
+const { TextArea } = Input
+export interface Props {
+}
+
+/**护士长月工作计划 */
+export default observer(function (props: Props) {
+
+  const onChange = (e: any, config: Obj) => {
+    const { index , key } = config
+    const newData = cloneDeep(model.editorData)
+    newData[index][key] = e.target.value
+    model.handleEditorChange(newData)
+  }
+
+
+  const removeObj = (idx:number)=>{
+    globalModal
+		.confirm( `提示`,`是否确认删除？`)
+		.then((res) => {
+      model.editorData.splice(idx,4)
+      message.success('删除成功！')
+     
+		}).catch(err=>{
+
+		})
+  }
+
+  return (
+    <Wrapper ref={model.ctxRef}  style={{ pointerEvents: model.allowEdit ? 'auto' : 'none' }}>
+      <div className='title'>护士受表彰及奖励记录</div>
+      <table>
+        <colgroup>
+          <col width='10%' />
+          <col width='5%' />
+          <col width='10%' />
+          <col width='15%' />
+          <col width='10%' />
+          <col width='10%' />
+          <col width='10%' />
+        </colgroup>
+        <thead>
+          <tr>
+            <th colSpan={4}>表彰类别</th>
+          </tr>
+          <tr>
+            <th rowSpan={3}>日期</th>
+            <th rowSpan={3}>姓名</th>
+            <th>嘉奖</th>
+            <th>立功</th>
+            <th>荣誉称号</th>
+            <th>其他</th>
+            <th rowSpan={3}>奖励方式</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            (model.editorData || []).map((v: Obj, i: number) => {
+              return (
+                <>
+                  <tr key={i}>
+                    {v.title !== undefined && <td rowSpan={4} className='ta-l' style={{position:'relative'}}>
+                    {(!model.isPrint && model.editorData.length/4>3) && <Button className='delete-btn' type='danger' shape="circle" size='small' icon="delete" onClick={()=>{removeObj(i)}}></Button>}
+                      <TextArea className='cell-ipt' autosize={{minRows: 4}} value={v.title} onChange={(e) => onChange(e, { index: i, key: 'title' })}></TextArea>
+                    </td>}
+                    <td>{i % 4 + 1}</td>
+                    <td className='ta-l'>
+                      <TextArea className='cell-ipt' autosize={{minRows: 1}} value={v.v1} onChange={(e) => onChange(e, { index: i, key: 'v1' })}></TextArea>
+                    </td>
+                    <td className='ta-l'>
+                      <Input className='cell-ipt' value={v.v2} onChange={(e) => onChange(e, { index: i, key: 'v2' })}></Input>
+                    </td>
+                    <td className='ta-l'>
+                      <Input className='cell-ipt' value={v.v3} onChange={(e) => onChange(e, { index: i, key: 'v3' })}></Input>
+                    </td>
+                    <td className='ta-l'>
+                      <Input className='cell-ipt' value={v.v4} onChange={(e) => onChange(e, { index: i, key: 'v4' })}></Input>
+                    </td>
+                    <td className='ta-l'>
+                      <TextArea className='cell-ipt' autosize={{minRows: 1}} value={v.v5} onChange={(e) => onChange(e, { index: i, key: 'v5' })}></TextArea>
+                    </td>
+                  </tr>
+                </>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    </Wrapper>
+  )
+})
+
+const Wrapper = styled(DetailCtxCon)`
+  th {
+    line-height: 32px;
+  }
+  .delete-btn{
+    position: absolute;
+    right: -5px;
+    top: -10px;
+    z-index: 2;
+  }
+  /* textarea {
+    min-height: 80px;
+    height: auto;
+  } */
+`
---
+++ src/modules/nurseHandBookNew/views/detail-jew/components/925HLLWDJ_9/925WCXXJXDJ_9_4.tsx
@@ -0,0 +1,111 @@
+import React from 'react'
+import styled from 'styled-components'
+import { nurseHandbookRecordModel as model } from '../../model'
+import { Button, Input,message} from 'antd'
+import { observer } from 'mobx-react'
+import { DetailCtxCon } from 'src/modules/nurseHandBookNew/style'
+import { Obj } from 'src/libs/types'
+import cloneDeep from 'lodash/cloneDeep'
+import { globalModal } from "src/global/globalModal"
+const { TextArea } = Input
+export interface Props {
+}
+
+/**护士长月工作计划 */
+export default observer(function (props: Props) {
+
+  const onChange = (e: any, config: Obj) => {
+    const { index , key } = config
+    const newData = cloneDeep(model.editorData)
+    newData[index][key] = e.target.value
+    model.handleEditorChange(newData)
+  }
+
+
+  const removeObj = (idx:number)=>{
+    globalModal
+		.confirm( `提示`,`是否确认删除？`)
+		.then((res) => {
+      model.editorData.splice(idx,4)
+      message.success('删除成功！')
+     
+		}).catch(err=>{
+
+		})
+  }
+
+  return (
+    <Wrapper ref={model.ctxRef}  style={{ pointerEvents: model.allowEdit ? 'auto' : 'none' }}>
+      <div className='title'>外出参观、进修、学习登记</div>
+      <table>
+        <colgroup>
+          <col width='12%' />
+          <col width='20%' />
+          <col width='16%' />
+          <col width='16%' />
+          <col width='16%' />
+          <col width='16%' />
+        </colgroup>
+        <thead>
+    
+          <tr>
+            <th>姓  名</th>
+            <th>进修及参观学习单位</th>
+            <th>离院时间</th>
+            <th>返院时间</th>
+            <th>总天数</th>
+            <th>获奖人</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            (model.editorData || []).map((v: Obj, i: number) => {
+              return (
+                <>
+                  <tr key={i}>
+                    {v.title !== undefined && <td rowSpan={4} className='ta-l' style={{position:'relative'}}>
+                    {(!model.isPrint && model.editorData.length/4>3) && <Button className='delete-btn' type='danger' shape="circle" size='small' icon="delete" onClick={()=>{removeObj(i)}}></Button>}
+                      <TextArea className='cell-ipt' autosize={{minRows: 4}} value={v.title} onChange={(e) => onChange(e, { index: i, key: 'title' })}></TextArea>
+                    </td>}
+                    <td>{i % 4 + 1}</td>
+                    <td className='ta-l'>
+                      <TextArea className='cell-ipt' autosize={{minRows: 1}} value={v.v1} onChange={(e) => onChange(e, { index: i, key: 'v1' })}></TextArea>
+                    </td>
+                    <td className='ta-l'>
+                      <Input className='cell-ipt' value={v.v2} onChange={(e) => onChange(e, { index: i, key: 'v2' })}></Input>
+                    </td>
+                    <td className='ta-l'>
+                      <Input className='cell-ipt' value={v.v3} onChange={(e) => onChange(e, { index: i, key: 'v3' })}></Input>
+                    </td>
+                    <td className='ta-l'>
+                      <Input className='cell-ipt' value={v.v4} onChange={(e) => onChange(e, { index: i, key: 'v4' })}></Input>
+                    </td>
+                    <td className='ta-l'>
+                      <TextArea className='cell-ipt' autosize={{minRows: 1}} value={v.v5} onChange={(e) => onChange(e, { index: i, key: 'v5' })}></TextArea>
+                    </td>
+                  </tr>
+                </>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    </Wrapper>
+  )
+})
+
+const Wrapper = styled(DetailCtxCon)`
+  th {
+    line-height: 32px;
+  }
+  .delete-btn{
+    position: absolute;
+    right: -5px;
+    top: -10px;
+    z-index: 2;
+  }
+  /* textarea {
+    min-height: 80px;
+    height: auto;
+  } */
+`
