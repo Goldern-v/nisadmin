3bab9740e897240c76fc2b27646a0286f404b332
else <15102082382@163.com>
2023-09-19 15:22:25 +0800

    fix：925--23036 【功能优化】档案管理操作增加内容

--- src/modules/nurseFiles/view/nurseFiles-wh/views/nurseFilesList/NurseFilesListView.tsx
+++ src/modules/nurseFiles/view/nurseFiles-wh/views/nurseFilesList/NurseFilesListView.tsx
@@ -1,7 +1,7 @@
 import BaseTable from 'src/components/BaseTable'
 import store, { appStore } from 'src/stores'
 import styled from 'styled-components'
-import React from 'react'
+import React, {useState} from 'react'
 import { RouteComponentProps } from 'react-router'
 import { observer } from 'mobx-react-lite'
 import { ColumnProps } from 'antd/lib/table'
@@ -13,6 +13,9 @@ import FilterConTree from './components/FilterConTree'
 import SelectCon from './components/SelectCon'
 
 import { nurseFilesListViewModel } from './NurseFilesListViewModel'
+import {Obj} from "src/libs/types";
+import {unstable_batchedUpdates} from "react-dom";
+import DeptModal from "src/modules/nurseFiles/view/nurseFiles-gzsrm/views/nurseFilesList/components/deptModal";
 
 const is925 = ['925', 'zjhj'].includes(appStore.HOSPITAL_ID)
 const is925Text = '925,zjhj'
@@ -245,292 +248,6 @@ const columns1: ColumnProps<any>[] = [
   }
 ]
 
-const columns: ColumnProps<any>[] = [
-  {
-    title: '科室',
-    dataIndex: 'deptName',
-    key: 'deptName',
-    width: ['lyrm', 'stmz', '925', 'zjhj'].includes(appStore.HOSPITAL_ID) ? 120 : 200,
-    align: 'left',
-    fixed: is925 ? 'left': false
-  },
-  {
-    title: '员工号',
-    dataIndex: 'empNo',
-    key: 'empNo',
-    width: 70,
-    align: 'center',
-    fixed: is925 ? 'left': false
-  },
-  {
-    title: '姓名',
-    dataIndex: 'empName',
-    key: 'empName',
-    width: 70,
-    align: 'center',
-    fixed: is925 ? 'left': false
-  },
-
-  {
-    title: '性别',
-    dataIndex: 'sex',
-    key: 'sex',
-    width: 50,
-    align: 'center',
-    render(sex: any) {
-      return sex == '0' ? '男' : sex == '1' ? '女' : sex
-    }
-  },
-  {
-    title: '年龄',
-    dataIndex: 'age',
-    key: 'age',
-    width: 50,
-    align: 'center'
-  },
-  {
-    title: '职称',
-    dataIndex: 'newTitle',
-    key: 'newTitle',
-    width: 90,
-    align: 'center'
-  },
-  {
-    title: '层级',
-    dataIndex: 'nurseHierarchy',
-    key: 'nurseHierarchy',
-    width: 70,
-    align: 'center'
-  },
-  {
-    title: '职务',
-    dataIndex: 'job',
-    key: 'job',
-    width: 120,
-    align: 'center'
-  },
-  {
-    title: '最高学历',
-    dataIndex: 'highestEducation',
-    key: 'highestEducation',
-    width: 80,
-    align: 'center'
-  },
-
-  {
-    title: '籍贯',
-    dataIndex: 'nativePlace',
-    key: 'nativePlace',
-    width: 100,
-    align: 'center'
-  },
-  {
-    title: '民族',
-    dataIndex: 'nation',
-    key: 'nation',
-    width: 70,
-    align: 'center'
-  },
-  ...appStore.hisMatch({
-    map: {
-      [is925Text]: [
-        {
-          title: '来院工作时间',
-          dataIndex: 'goHospitalWorkDate',
-          key: 'goHospitalWorkDate',
-          width: 100,
-          align: 'center'
-        },
-      ],
-      other: []
-    },
-    vague: true
-  }),
-  {
-    title: '护士执业证书编号',
-    dataIndex: 'zyzsNumber',
-    key: 'zyzsNumber',
-    width: 150,
-    align: 'center'
-  },
-  {
-    title: '执业证书截至日期',
-    dataIndex: 'zyzsEffectiveUpDate',
-    key: 'zyzsEffectiveUpDate',
-    width: 120,
-    align: 'center'
-  },
-
-  {
-    title: '政治面貌',
-    dataIndex: 'politicsLook',
-    key: 'politicsLook',
-    width: 120,
-    align: 'center'
-  },
-  {
-    title: '来院工作年限',
-    dataIndex: 'goHospitalWorkYear',
-    key: 'goHospitalWorkYear',
-    width: 100,
-    align: 'center',
-    render(text: any, record: any) {
-      return (text || 0) + '年'
-    }
-  },
-  appStore.HOSPITAL_ID === 'gxjb' ?
-    {
-      title: '家庭住址',
-      dataIndex: 'address',
-      key: 'address',
-      width: 180,
-      align: 'center'
-    } :
-    {
-      title: '鞋码',
-      dataIndex: 'shoeSize',
-      key: 'shoeSize',
-      width: 80,
-      align: 'center'
-    },
-    is925 ?
-  {
-    title: '身高(cm)',
-    dataIndex: 'height',
-    key: 'height',
-    width: 110,
-    align: 'center'
-  } : {
-    title: '编制',
-    dataIndex: 'workConversion',
-    key: 'workConversion',
-    width: 100,
-    align: 'center'
-  },
-  ['qhwy', 'whhk', 'dglb', 'dghm'].includes(appStore.HOSPITAL_ID) ?
-  {
-    title: '护理学会会员证号',
-    dataIndex: 'membershipCardNumber',
-    key: 'membershipCardNumber',
-    width: 110,
-    align: 'center'
-  } : {},
-  {
-    title: '身份证号',
-    dataIndex: 'cardNumber',
-    key: 'cardNumber',
-    width: 160,
-    align: 'center'
-  },
-  {
-    title: '手机号',
-    dataIndex: 'phone',
-    key: 'phone',
-    width: 100,
-    align: 'center'
-  },
-  {
-    title: '取得执业证书并从事护理岗位时间',
-    dataIndex: 'zyzsNursingPostDate',
-    key: 'zyzsNursingPostDate',
-    width: 100,
-    align: 'center'
-  },
-  {
-    title: '现职务任职起始时间',
-    dataIndex: 'jobStartDate',
-    key: 'jobStartDate',
-    width: 100,
-    align: 'center'
-  },
-  
-  ...appStore.hisMatch({
-    map: {
-      [is925Text]: [],
-      other: [
-        {
-          title: '院内工作地点',
-          dataIndex: 'workAddress',
-          key: 'workAddress',
-          width: 100,
-          align: 'center'
-        },
-      ]
-    },
-    vague: true
-  }),
-  ...appStore.hisMatch({
-    map: {
-      fsxt: [
-        {
-          title: '在读学历',
-          key: 'current_education_background',
-          width: 100,
-          align: 'center',
-          render(text: any, record: any) {
-            return record.nurseExpand.current_education_background
-          }
-        },
-        {
-          title: '已取得最高学历毕业时间',
-          key: 'last_education_graduation_time',
-          width: 100,
-          align: 'center',
-          render(text: any, record: any) {
-            return record.nurseExpand.last_education_graduation_time
-          }
-        },
-        {
-          title: '执业注册有效期',
-          key: 'licensed_of_practice_time',
-          width: 100,
-          align: 'center',
-          render(text: any, record: any) {
-            return record.nurseExpand.licensed_of_practice_time
-          }
-        },
-        {
-          title: '资格证书编号',
-          key: 'hdry_qua_cer_no',
-          width: 100,
-          align: 'center',
-          render(text: any, record: any) {
-            return record.nurseExpand.hdry_qua_cer_no
-          }
-        },
-        {
-          title: '来院工作时间',
-          dataIndex: 'goHospitalWorkDate',
-          key: 'goHospitalWorkDate',
-          width: 100,
-          align: 'center'
-        },
-        {
-          title: '参加工作时间',
-          dataIndex: 'goWorkTime',
-          key: 'goWorkTime',
-          width: 100,
-          align: 'center'
-        },
-      ],
-      other: []
-    }
-  }),
-  {
-    title: '操作',
-    dataIndex: 'auditedStatusName',
-    key: '6',
-    width: 70,
-    align: 'center',
-    render(text: any, row: any) {
-      return (
-        <DoCon>
-          <span onClick={() => row.empName && onDoubleClick(row)}>查看</span>
-        </DoCon>
-      )
-    }
-  }
-]
 
 const onDoubleClick = (record: any) => {
   localStorage.setItem('empName', record.empName)
@@ -538,6 +255,311 @@ const onDoubleClick = (record: any) => {
 }
 
 export default observer(function NurseFilesListView() {
+  const columns: ColumnProps<any>[] = [
+    {
+      title: '科室',
+      dataIndex: 'deptName',
+      key: 'deptName',
+      width: ['lyrm', 'stmz', '925', 'zjhj'].includes(appStore.HOSPITAL_ID) ? 120 : 200,
+      align: 'left',
+      fixed: is925 ? 'left': false
+    },
+    {
+      title: '员工号',
+      dataIndex: 'empNo',
+      key: 'empNo',
+      width: 70,
+      align: 'center',
+      fixed: is925 ? 'left': false
+    },
+    {
+      title: '姓名',
+      dataIndex: 'empName',
+      key: 'empName',
+      width: 70,
+      align: 'center',
+      fixed: is925 ? 'left': false
+    },
+
+    {
+      title: '性别',
+      dataIndex: 'sex',
+      key: 'sex',
+      width: 50,
+      align: 'center',
+      render(sex: any) {
+        return sex == '0' ? '男' : sex == '1' ? '女' : sex
+      }
+    },
+    {
+      title: '年龄',
+      dataIndex: 'age',
+      key: 'age',
+      width: 50,
+      align: 'center'
+    },
+    {
+      title: '职称',
+      dataIndex: 'newTitle',
+      key: 'newTitle',
+      width: 90,
+      align: 'center'
+    },
+    {
+      title: '层级',
+      dataIndex: 'nurseHierarchy',
+      key: 'nurseHierarchy',
+      width: 70,
+      align: 'center'
+    },
+    {
+      title: '职务',
+      dataIndex: 'job',
+      key: 'job',
+      width: 120,
+      align: 'center'
+    },
+    {
+      title: '最高学历',
+      dataIndex: 'highestEducation',
+      key: 'highestEducation',
+      width: 80,
+      align: 'center'
+    },
+
+    {
+      title: '籍贯',
+      dataIndex: 'nativePlace',
+      key: 'nativePlace',
+      width: 100,
+      align: 'center'
+    },
+    {
+      title: '民族',
+      dataIndex: 'nation',
+      key: 'nation',
+      width: 70,
+      align: 'center'
+    },
+    ...appStore.hisMatch({
+      map: {
+        [is925Text]: [
+          {
+            title: '来院工作时间',
+            dataIndex: 'goHospitalWorkDate',
+            key: 'goHospitalWorkDate',
+            width: 100,
+            align: 'center'
+          },
+        ],
+        other: []
+      },
+      vague: true
+    }),
+    {
+      title: '护士执业证书编号',
+      dataIndex: 'zyzsNumber',
+      key: 'zyzsNumber',
+      width: 150,
+      align: 'center'
+    },
+    {
+      title: '执业证书截至日期',
+      dataIndex: 'zyzsEffectiveUpDate',
+      key: 'zyzsEffectiveUpDate',
+      width: 120,
+      align: 'center'
+    },
+
+    {
+      title: '政治面貌',
+      dataIndex: 'politicsLook',
+      key: 'politicsLook',
+      width: 120,
+      align: 'center'
+    },
+    {
+      title: '来院工作年限',
+      dataIndex: 'goHospitalWorkYear',
+      key: 'goHospitalWorkYear',
+      width: 100,
+      align: 'center',
+      render(text: any, record: any) {
+        return (text || 0) + '年'
+      }
+    },
+    appStore.HOSPITAL_ID === 'gxjb' ?
+        {
+          title: '家庭住址',
+          dataIndex: 'address',
+          key: 'address',
+          width: 180,
+          align: 'center'
+        } :
+        {
+          title: '鞋码',
+          dataIndex: 'shoeSize',
+          key: 'shoeSize',
+          width: 80,
+          align: 'center'
+        },
+    is925 ?
+        {
+          title: '身高(cm)',
+          dataIndex: 'height',
+          key: 'height',
+          width: 110,
+          align: 'center'
+        } : {
+          title: '编制',
+          dataIndex: 'workConversion',
+          key: 'workConversion',
+          width: 100,
+          align: 'center'
+        },
+    ['qhwy', 'whhk', 'dglb', 'dghm'].includes(appStore.HOSPITAL_ID) ?
+        {
+          title: '护理学会会员证号',
+          dataIndex: 'membershipCardNumber',
+          key: 'membershipCardNumber',
+          width: 110,
+          align: 'center'
+        } : {},
+    {
+      title: '身份证号',
+      dataIndex: 'cardNumber',
+      key: 'cardNumber',
+      width: 160,
+      align: 'center'
+    },
+    {
+      title: '手机号',
+      dataIndex: 'phone',
+      key: 'phone',
+      width: 100,
+      align: 'center'
+    },
+    {
+      title: '取得执业证书并从事护理岗位时间',
+      dataIndex: 'zyzsNursingPostDate',
+      key: 'zyzsNursingPostDate',
+      width: 100,
+      align: 'center'
+    },
+    {
+      title: '现职务任职起始时间',
+      dataIndex: 'jobStartDate',
+      key: 'jobStartDate',
+      width: 100,
+      align: 'center'
+    },
+
+    ...appStore.hisMatch({
+      map: {
+        [is925Text]: [],
+        other: [
+          {
+            title: '院内工作地点',
+            dataIndex: 'workAddress',
+            key: 'workAddress',
+            width: 100,
+            align: 'center'
+          },
+        ]
+      },
+      vague: true
+    }),
+    ...appStore.hisMatch({
+      map: {
+        fsxt: [
+          {
+            title: '在读学历',
+            key: 'current_education_background',
+            width: 100,
+            align: 'center',
+            render(text: any, record: any) {
+              return record.nurseExpand.current_education_background
+            }
+          },
+          {
+            title: '已取得最高学历毕业时间',
+            key: 'last_education_graduation_time',
+            width: 100,
+            align: 'center',
+            render(text: any, record: any) {
+              return record.nurseExpand.last_education_graduation_time
+            }
+          },
+          {
+            title: '执业注册有效期',
+            key: 'licensed_of_practice_time',
+            width: 100,
+            align: 'center',
+            render(text: any, record: any) {
+              return record.nurseExpand.licensed_of_practice_time
+            }
+          },
+          {
+            title: '资格证书编号',
+            key: 'hdry_qua_cer_no',
+            width: 100,
+            align: 'center',
+            render(text: any, record: any) {
+              return record.nurseExpand.hdry_qua_cer_no
+            }
+          },
+          {
+            title: '来院工作时间',
+            dataIndex: 'goHospitalWorkDate',
+            key: 'goHospitalWorkDate',
+            width: 100,
+            align: 'center'
+          },
+          {
+            title: '参加工作时间',
+            dataIndex: 'goWorkTime',
+            key: 'goWorkTime',
+            width: 100,
+            align: 'center'
+          },
+        ],
+        other: []
+      }
+    }),
+    {
+      title: '操作',
+      dataIndex: 'auditedStatusName',
+      key: '6',
+      width:appStore.HOSPITAL_ID ==='925' ? 170 : 70,
+      align: 'center',
+      render(text: any, row: any) {
+        return (
+            <DoCon>
+              <span onClick={() => row.empName && onDoubleClick(row)}>查看</span>
+              { appStore.HOSPITAL_ID ==='925' &&<>
+                <span onClick={() => openModal(row, 'dept')}>护理单元配置</span>
+                <span onClick={() => openModal(row, 'role')}>角色设置</span>
+              </> }
+
+            </DoCon>
+        )
+      }
+    }
+  ]
+  const [deptModalVis, setDeptModalVis] = useState<boolean>(false)
+  const [curItem, setCurItem] = useState<Obj>({})
+  const [modalType, setModalType] = useState('')
+  const openModal = (record: Obj, flag: string) => {
+    unstable_batchedUpdates(() => {
+      setDeptModalVis(true)
+      setCurItem(record)
+      setModalType(flag)
+    })
+  }
+  const onDeptModalOk = () => {
+    setDeptModalVis(false)
+    nurseFilesListViewModel.loadNursingList();
+  }
   return (
     <Wrapper>
       <SelectCon />
@@ -572,6 +594,12 @@ export default observer(function NurseFilesListView() {
         }}
         loading={nurseFilesListViewModel.listSpinning}
       />
+      <DeptModal
+          visible={deptModalVis}
+          modalType={modalType}
+          handleOk={onDeptModalOk}
+          handleCancel={() => { setDeptModalVis(false) }}
+          curItem={curItem} />
     </Wrapper>
   )
 })
