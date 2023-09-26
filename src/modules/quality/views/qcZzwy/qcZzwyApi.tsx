5c63f974721cb8356a28f5dee26a25788b1de541
else <15102082382@163.com>
2023-09-26 13:44:10 +0800

    fix：烟台玲珑--新版敏感指标

--- src/modules/quality/views/qcZzwy/qcZzwyApi.tsx
+++ src/modules/quality/views/qcZzwy/qcZzwyApi.tsx
@@ -3,18 +3,20 @@ import qs from "qs";
 
 export default class QcZzwyApi extends BaseApiService {
 
-  // 护理部质量检查汇总表
-	public async getInspectionSummary(obj: any) {
-		return this.post(`/qcItem/getInspectionSummary`,obj);
-	}
-	// 质控表项目问题分析汇总-获取模板列表
-	public async getTemplateList(qcLevel: any) {
-		return this.get(`/qcTemplateManage/templateList/${qcLevel}`);
-	}
-	// 质控表项目问题分析汇总
-	public async getRectificationdeptSummary(obj: any) {
-		return this.get(`/qcItem/getRectificationdeptSummary?${qs.stringify(obj)}`);
-	}
+    // 护理部质量检查汇总表
+    public async getInspectionSummary(obj: any) {
+        return this.post(`/qcItem/getInspectionSummary`, obj);
+    }
+
+    // 质控表项目问题分析汇总-获取模板列表
+    public async getTemplateList(qcLevel: any) {
+        return this.get(`/qcTemplateManage/templateList/${qcLevel}`);
+    }
+
+    // 质控表项目问题分析汇总
+    public async getRectificationdeptSummary(obj: any) {
+        return this.get(`/qcItem/getRectificationdeptSummary?${qs.stringify(obj)}`);
+    }
 
     // 导出年度汇总表
     public async exportSumYearTable(obj: any) {
@@ -34,70 +36,79 @@ export default class QcZzwyApi extends BaseApiService {
 
     /**质控检查反馈整改单汇总**/
 
-    public getRectificationSummary(params:any) {
+    public getRectificationSummary(params: any) {
         return this.get(`/qcItem/getRectificationSummary?wardCode=${params.wardCode}&beginDate=${params.beginDate}&endDate=${params.endDate}&qcLevel=${params.qcLevel}`)
     }
 
     /**编辑整改措施和追踪评价**/
-    public saveOrUpdateContent(params:any) {
-        return this.post('/qcItem/rectificationSummary/saveOrUpdateContent',params)
+    public saveOrUpdateContent(params: any) {
+        return this.post('/qcItem/rectificationSummary/saveOrUpdateContent', params)
     }
+
     /**获取分页报告**/
-    public qcReportGetPage(params:any) {
-        return this.post('/qcReport/getPage',params)
+    public qcReportGetPage(params: any) {
+        return this.post('/qcReport/getPage', params)
     }
+
     /**创建分析报告**/
-    public createQcReport(params:any) {
-        return this.post('/qcReport/createQcReport',params)
+    public createQcReport(params: any) {
+        return this.post('/qcReport/createQcReport', params)
     }
+
     /**删除报告**/
-    public deleteQcReport(params:any) {
-        return this.post('/qcReport/deleteQcReport',params)
+    public deleteQcReport(params: any) {
+        return this.post('/qcReport/deleteQcReport', params)
     }
+
     /**修改报告**/
-    public updateQcReport(params:any) {
-        return this.post('/qcReport/updateQcReport',params)
+    public updateQcReport(params: any) {
+        return this.post('/qcReport/updateQcReport', params)
     }
-/**查看报告**/
-    public getQcReportById(reportId:number) {
+
+    /**查看报告**/
+    public getQcReportById(reportId: number) {
         return this.get(`/qcReport/getQcReport/${reportId}`)
     }
-/**保存报告**/
-public saveQcReport(params:number) {
-    return this.post(`/qcReport/saveQcReport`,params)
-}
-/**保存报告**/
-public upload(obj:any) {
-    const trancePostData = new FormData();
-    for (let key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        trancePostData.append(key, obj[key]);
-      }
-    }
-    // return this.post(`/file/uploadNurse`, trancePostData);
-    return this.post(`/qcReport/upload`,trancePostData)
-}
-/**季度质量分析报告（一级、二级项目内容）
- * @param params.beginDate {string}
- * @param params.endDate {string}
- * @param params.wardCode {string}
- * @param params.qcCode {string}
- * @param params.qcItemLevel {string}
- *
- * **/
-public getQcItemDataList(params:any) {
-    return this.post(`/qcReport/getQcItemDataList`,params)
-}
 
-/***删除
- *
- * {
- *     "reportId": "string",
- * }
- * **/
-public qcItemDeleteQcReport(params:any) {
-    return this.post(`/qcReport/deleteQcReport`,params)
-}
+    /**保存报告**/
+    public saveQcReport(params: number) {
+        return this.post(`/qcReport/saveQcReport`, params)
+    }
+
+    /**保存报告**/
+    public upload(obj: any) {
+        const trancePostData = new FormData();
+        for (let key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                trancePostData.append(key, obj[key]);
+            }
+        }
+        // return this.post(`/file/uploadNurse`, trancePostData);
+        return this.post(`/qcReport/upload`, trancePostData)
+    }
+
+    /**季度质量分析报告（一级、二级项目内容）
+     * @param params.beginDate {string}
+     * @param params.endDate {string}
+     * @param params.wardCode {string}
+     * @param params.qcCode {string}
+     * @param params.qcItemLevel {string}
+     *
+     * **/
+    public getQcItemDataList(params: any) {
+        return this.post(`/qcReport/getQcItemDataList`, params)
+    }
+
+    /***删除
+     *
+     * {
+     *     "reportId": "string",
+     * }
+     * **/
+    public qcItemDeleteQcReport(params: any) {
+        return this.post(`/qcReport/deleteQcReport`, params)
+    }
+
     /**二级指标的上个月的数据与之比对
      * {
      *     "wardCode": "string",
@@ -111,14 +122,25 @@ public qcItemDeleteQcReport(params:any) {
      *
      **/
 
-        /**根据报告表Code找到报告表下的二级项目 */
-        public getReportTwoItem(qcCode:any) {
-            return this.get(`/qcReport/getReportTwoItem/${qcCode}`)
-        }
+    /**根据报告表Code找到报告表下的二级项目 */
+    public getReportTwoItem(qcCode: any) {
+        return this.get(`/qcReport/getReportTwoItem/${qcCode}`)
+    }
 
-        /**二级指标的上个月的数据与之比对 */
-        public getRatioByItemCode(params:any) {
-            return this.post(`/qcReport/getRatioByItemCode`,params)
-        }
+    /**二级指标的上个月的数据与之比对 */
+    public getRatioByItemCode(params: any) {
+        return this.post(`/qcReport/getRatioByItemCode`, params)
+    }
+   /** 护理部质量检查汇总表  导出
+    *  "wardCode": "全院",
+    *  "beginDate": "2023-01-20",
+    *  "endDate": "2023-12-31",
+    *  "pageIndex": 1,
+    *  "pageSize": 20,
+    *  "level": "1"
+    * **/
+   public inspectionSummary(params: any) {
+       return this.post(`/qcItem/inspectionSummary/export`, params)
+   }
 }
 export const qcZzwyApi = new QcZzwyApi();
