f571f0e48fa3330c2c648ec90db9b852fd0c43ae
else <15102082382@163.com>
2023-09-28 15:00:52 +0800

    fix：武汉亚心--24569 【BUG】规培手册在外网可以正常查看

--- src/modules/continuingEdu/views/gaugePearson/handbook/components/template3.tsx
+++ src/modules/continuingEdu/views/gaugePearson/handbook/components/template3.tsx
@@ -1,25 +1,34 @@
-import { observer } from 'mobx-react'
-import React from 'react'
+import {observer} from 'mobx-react'
+import React, {useEffect, useState} from 'react'
 import styled from 'styled-components'
 import {handbookModel} from "src/modules/continuingEdu/views/gaugePearson/handbook/model"
 import PdfViewer from "src/modules/nursingRulesNew-wh/components/PdfViewer";
 
 /**附件 */
 export default observer(function Template3() {
+    const [replacedPdfLink, setReplacedPdfLink] = useState('')
+    useEffect(() => {
+        if(handbookModel.detail?.attachment?.path){
+            const currentDomain = `${window.location.protocol}//${window.location.host}`;
+            const pdfLink = handbookModel.detail.attachment.path;
+            const url:string =pdfLink.replace(/^(https?:\/\/[^/]+)/, currentDomain)
+            setReplacedPdfLink(url);
+        }
+    }, [handbookModel.detail.attachment?.path])
     return (
-    <Wrapper>
-        <PdfViewer file={handbookModel?.detail?.attachment?.path} width={780 - 2} />
-      {/*<iframe src={handbookModel?.detail?.attachment?.path} />*/}
-    </Wrapper>
-  )
+        <Wrapper>
+            <PdfViewer file={replacedPdfLink} width={780 - 2}/>
+        </Wrapper>
+    )
 })
 
 const Wrapper = styled.div`
-height: 100%;
-  width: 80%;
-iframe {
-  width: 100%;
   height: 100%;
-  border: none;
-}
+  width: 80%;
+
+  iframe {
+    width: 100%;
+    height: 100%;
+    border: none;
+  }
 `
