dc09b582d2afc24a9d330876cc300caa0cab5b87
else <15102082382@163.com>
2023-10-26 17:45:36 +0800

    fix：湛江海军--护士长手册内容空白，临邑--专科准入，江门妇幼--质控-月度分析报告

--- src/modules/nurseHandBookNew/views/detail-jew/components/editPage.tsx
+++ src/modules/nurseHandBookNew/views/detail-jew/components/editPage.tsx
@@ -10,39 +10,48 @@ CKEditor.editorUrl = `ckeditor/ckeditor.js`
 
 export interface Props {
 }
+let editorInstance:any;
 export default observer(function (props: Props, ref) {
-  const editorRef = useRef()
+  // const editorRef = useRef()
 
   const onChange = (e: any) => {
+    console.log(e.editor.getData());
     model.handleEditorChange({ v1: e.editor.getData() })
   }
-useEffect(()=>{
-  console.log("model.editorData.v1====",model.editorData);
-},[model.editorData])
+  useEffect(() => {
+    // 销毁 CKEditor 实例
+    return () => {
+      if (editorInstance) {
+        editorInstance.destroy();
+      }
+    };
+    }, []);
   return (
-    <Wrapper ref={model.ctxRef}  style={{ pointerEvents: model.allowEdit ? 'auto' : 'none' }}>
-      <Input className='title' value={model.editorTitle} onChange={(e) => model.onChangeTitle(e)} />
-      {model.isPrint ?
-      <div dangerouslySetInnerHTML={{ __html: model.editorData.v1}}></div>
-      :
-      <CKEditor
-        ref={editorRef}
-        data={model.editorData.v1}
-        name='editor1'
-        onChange={onChange}
-        config={{
-          extraPlugins: 'stylesheetparser,colorbutton,colordialog,html5video',
-          removePlugins: 'easyimage,cloudservices',
-          filebrowserUploadUrl: appStore.uploadCKEUrl,
-          filebrowserHtml5videoUploadUrl: appStore.uploadCKEUrl,
-          height: 400,
-          title: ''
-        }}
-      />}
-    </Wrapper>
+      <Wrapper ref={model.ctxRef}  style={{ pointerEvents: model.allowEdit ? 'auto' : 'none' }}>
+        <Input className='title' value={model.editorTitle} onChange={(e) => model.onChangeTitle(e)} />
+        {model.isPrint ?
+            <div dangerouslySetInnerHTML={{ __html: model.editorData.v1}}></div>
+            :
+            <CKEditor
+                onInstanceReady={(editor:any) => {
+                  // 将 CKEditor 4 实例赋值给全局变量
+                  editorInstance = editor.editor;
+                }}
+                data={model.editorData.v1}
+                name='editor1'
+                onChange={onChange}
+                config={{
+                  extraPlugins: 'stylesheetparser,colorbutton,colordialog,html5video',
+                  removePlugins: 'easyimage,cloudservices',
+                  filebrowserUploadUrl: appStore.uploadCKEUrl,
+                  filebrowserHtml5videoUploadUrl: appStore.uploadCKEUrl,
+                  height: 400,
+                  title: ''
+                }}
+            />}
+      </Wrapper>
   )
 })
-
 const Wrapper = styled(DetailCtxCon)`
   .title {
     border-left: none;
